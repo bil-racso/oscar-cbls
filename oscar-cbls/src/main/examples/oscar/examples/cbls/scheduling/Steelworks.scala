c8cf5d6e8b2d906f448469110220073c7609c43d
Renaud De Landtsheer <renaud.delandtsheer@cetic.be>
2014-11-24 16:30:25 +0100

    put SteelWork into exemples, and removed some useless parameters to the store.

--- oscar-cbls/src/main/examples/oscar/examples/cbls/scheduling/Steelworks.scala
+++ oscar-cbls/src/main/examples/oscar/examples/cbls/scheduling/Steelworks.scala
@@ -12,7 +12,7 @@
  * You should have received a copy of the GNU Lesser General Public License along with OscaR.
  * If not, see http://www.gnu.org/licenses/lgpl-3.0.en.html
  ******************************************************************************/
-package oscar.cbls.test.scheduling
+package oscar.examples.cbls.scheduling
 
 import oscar.cbls.invariants.core.computation.CBLSIntVar.int2IntVar
 import oscar.cbls.invariants.core.computation.Store
