8fc554908b4fa724e60cdf51c975027c2e2f56d7
rhartert <devnull@localhost>
2014-12-24 14:07:28 +0100

    Remove the feedback functionnality of Inconsistency to prevent bugs in multi-threading.

--- oscar-cp/src/main/scala/oscar/cp/core/Inconsistency.scala
+++ oscar-cp/src/main/scala/oscar/cp/core/Inconsistency.scala
@@ -4,28 +4,10 @@ package oscar.cp.core
 
 abstract class Inconsistency extends Exception {
   
-  def feedback: AnyRef
-  
   // Do not fill the trace
   final override val fillInStackTrace: Throwable = this 
   
-  final override def toString: String = {
-    if (feedback == null) "Inconsistency"
-    else s"Inconsistency: $feedback"
-  }
+  final override val toString: String = "Inconsistency"
 }
 
-object Inconsistency extends Inconsistency {  
-  
-  // Feedback message (useful for debugging)
-  final private var _feedback: AnyRef = null 
-  
-  final override def feedback: AnyRef = _feedback 
-  
-  final def apply(): Inconsistency = apply(null)  
-  
-  final def apply(feedback: AnyRef): Inconsistency = {
-    _feedback = feedback
-    this
-  }
-}
+object Inconsistency extends Inconsistency
