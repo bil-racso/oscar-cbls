88183132d02e1dfbff605277958f3d962fa78d0d
rhartert <devnull@localhost>
2015-01-07 21:40:40 +0100

    Clean RCPSP example

--- oscar-cp/src/main/examples/oscar/examples/cp/RCPSP.scala
+++ oscar-cp/src/main/examples/oscar/examples/cp/RCPSP.scala
@@ -1,22 +1,8 @@
 package oscar.examples.cp
 
 import oscar.cp._
-import oscar.cp.scheduling.constraints.TTPerTask
-import oscar.cp.constraints.scheduling.LinearTT
 
 object RCPSP extends CPModel with App {
-  
-  /*
-  test LinearTT instance 4: failed !
-  expected number of solutions: 12
-  number of solutions: 4
-  INSTANCE
-  durations: 3, 2
-  demands: 3, 0
-  resources: 0, 0
-  capacity: 4
-  horizon: 5
-  */
   
   // (duration, consumption)
   val instance = Array((50, 1), (30, 1), (90, 3), (10, 2), (20, 2), (80, 1), (30, 2), (20, 2), (20, 1), (10, 1), (10, 2), (20, 2), (80, 1))
@@ -34,23 +20,7 @@ object RCPSP extends CPModel with App {
   val demands = Array.tabulate(nTasks)(t => CPIntervalVar(demandsData(t)))
   val makespan = maximum(ends)
   
-  val resources = Array.fill(nTasks)(CPIntervalVar(0))
-  val capacity = CPIntervalVar(capa)
-  
-  val mirrorStart = ends.map(-_)
-  val mirrorEnd = starts.map(-_)
-  
-  // nNodes: 1445964
-  // nFails: 722983
-  
-  // nNodes: 2570984
-  // nFails: 1285493
-  
-  //add(TTPerTask(starts, durations, ends, demands, resources, capacity, 0))
-  
-  add(new LinearTT(starts, durationsData, ends, demandsData, capa))
-  
-  //add(maxCumulativeResource(starts, durations, ends, demands, CPIntervalVar(capa)),Weak)
+  add(maxCumulativeResource(starts, durations, ends, demands, CPIntervalVar(capa)), Weak)
   
   minimize(makespan) 
   
