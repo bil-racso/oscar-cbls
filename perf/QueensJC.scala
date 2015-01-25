bd8540f2b94d74fbaac67b2e8472a36463464501
rhartert <devnull@localhost>
2015-01-25 15:40:10 +0100

    Update Queens performance model

--- perf/QueensJC.scala
+++ perf/QueensJC.scala
@@ -1,17 +1,19 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  * OscaR is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 2.1 of the License, or
  * (at your option) any later version.
- *   
+ *
  * OscaR is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License  for more details.
- *   
+ *
  * You should have received a copy of the GNU Lesser General Public License along with OscaR.
  * If not, see http://www.gnu.org/licenses/lgpl-3.0.en.html
- ******************************************************************************/
+ * ****************************************************************************
+ */
 
 import oscar.cp._
 import oscar.algo.search.Branching
@@ -20,30 +22,27 @@ import oscar.algo.search.Branching
  * n-queens model: place n-queens on a chess-board such that they don't attack each other.
  * @author Pierre Schaus pschaus@gmail.com
  */
-object QueensJC {
-  def main(args: Array[String]) {
+object QueensJC extends CPModel with App {
 
-    val cp = CPSolver()
-    cp.silent = true
-    val n = 88 //number of queens
-    val Queens = 0 until n
-    //variables
-    val queens = for (i <- Queens) yield CPIntVar.sparse(0, n-1)(cp)
+  solver.silent = true
 
-    var nbsol = 0
+  val nQueens = 88 // Number of queens
+  val Queens = 0 until nQueens
 
-    val cl = Weak
-    cp.add(allDifferent(queens), Weak)
-    cp.add(allDifferent(for (i <- Queens) yield queens(i) + i), Weak)
-    cp.add(allDifferent(for (i <- Queens) yield queens(i) - i), Weak)
-    
-    cp.search {
-      binaryFirstFail(queens)
-    }
-    
-    println(cp.start(nSols=1))
+  // Variables
+  val queens = Array.fill(nQueens)(CPIntVar.sparse(0, nQueens - 1))
 
-  }
+  // Constraints
+  add(allDifferent(queens))
+  add(allDifferent(Queens.map(i => queens(i) + i)))
+  add(allDifferent(Queens.map(i => queens(i) - i)))
+
+  // Search heuristic
+  search(binaryFirstFail(queens))
+
+  // Execution
+  val stats = start(nSols = 1)
+  println(stats)
 }
 
 
--- perf/QueensJCDecomp.scala
+++ perf/QueensJCDecomp.scala
@@ -1,17 +1,19 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  * OscaR is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 2.1 of the License, or
  * (at your option) any later version.
- *   
+ *
  * OscaR is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License  for more details.
- *   
+ *
  * You should have received a copy of the GNU Lesser General Public License along with OscaR.
  * If not, see http://www.gnu.org/licenses/lgpl-3.0.en.html
- ******************************************************************************/
+ * ****************************************************************************
+ */
 
 import oscar.cp._
 import oscar.algo.search.Branching
@@ -20,39 +22,31 @@ import oscar.algo.search.Branching
  * n-queens model: place n-queens on a chess-board such that they don't attack each other.
  * @author Pierre Schaus pschaus@gmail.com
  */
-object QueensJCDecomp {
-  
-  def main(args: Array[String]) {
+object QueensJCDecomp extends CPModel with App {
 
-    val cp = CPSolver()
-    cp.silent = true
-    val n = 88 //number of queens
-    val Queens = 0 until n
-    //variables
-    val queens = for (i <- Queens) yield CPIntVar.sparse(0, n-1)(cp)
+  solver.silent = true
 
-    var nbsol = 0
+  val nQueens = 88 // Number of queens
+  val Queens = 0 until nQueens
 
-    val cl = Weak
-    for (i <- 0 until n; j <- 0 until i) {
-      cp.add(queens(i) != queens(j))
-      cp.add(queens(i)+i != queens(j)+j)
-      cp.add(queens(i)-i != queens(j)-j)
-    }
-    
-    cp.search {
-      binaryFirstFail(queens)
-    }
-    
-    cp.onSolution {
-      println(queens.mkString(","))
-    }
-    
-    println(cp.start(nSols=1))
-    println(cp.statistics)
+  // Variables
+  val queens = Array.fill(nQueens)(CPIntVar.sparse(0, nQueens - 1))
+  val rQueens = Array.tabulate(nQueens)(i => queens(i) + i)
+  val fQueens = Array.tabulate(nQueens)(i => queens(i) - i)
 
-  }  
- 
+  // Constraints
+  for (i <- Queens; j <- Queens; if j < i) {
+    add(queens(i) != queens(j))
+    add(rQueens(i) != rQueens(j))
+    add(fQueens(i) != fQueens(j))
+  }
+
+  // Search heuristic
+  search(binaryFirstFail(queens))
+
+  // Execution
+  val stats = start(nSols = 1)
+  println(stats)
 }
 
 
