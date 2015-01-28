4d9dc40ea36ac49deaf1c93a84b2bec93662f5e6
pschaus <devnull@localhost>
2015-01-28 14:58:23 +0100

    make large scale scheduling deterministic reading file

--- perf/LargeScaleGreedyCumulative.scala
+++ perf/LargeScaleGreedyCumulative.scala
@@ -14,38 +14,49 @@
  *******************************************************************************/
 
 import oscar.cp._
+import scala.io.Source
 
 /** @author Pierre Schaus */
 object LargeScaleGreedyCumulative extends CPModel with App {
 
-  val nTasks = 6000
-  val rand = new scala.util.Random(0)
+  val lines = Source.fromFile("../data/large_scale_scheduling_01.txt").getLines.reduceLeft(_ + " " + _)
+  val vals = lines.split("[ ,\t]").toList.filterNot(_ == "").map(_.toInt)
+  var index = 0
+  def next() = {
+    index += 1
+    vals(index - 1)
+  }
 
-  val durations = Array.fill(nTasks)(rand.nextInt(200) + 5)
-  val height = Array.fill(nTasks)(rand.nextInt(40) + 1)
-  val horizon = 1000000000
-  val capa = 100
+  val nTasks = next()
+  val capa = next()
   
-  val durationsVar = Array.tabulate(nTasks)(t => CPIntervalVar(durations(t)))
-  val startsVar = Array.tabulate(nTasks)(t => CPIntervalVar(0, horizon))
+  val durations = Array.fill(nTasks)(next())
+  val height = Array.fill(nTasks)(next())
+  val horizon = durations.sum
+
+  
+  val durationsVar = Array.tabulate(nTasks)(t => CPIntVar(durations(t)))
+  val startsVar = Array.tabulate(nTasks)(t => CPIntVar(0, horizon))
   val endsVar = Array.tabulate(nTasks)(t => startsVar(t) + durationsVar(t))
-  val demandsVar = Array.tabulate(nTasks)(t => CPIntervalVar(height(t)))
+  val demandsVar = Array.tabulate(nTasks)(t => CPIntVar(height(t)))
   val capacity = CPIntVar(capa)
 
   add(maxCumulativeResource(startsVar, durationsVar, endsVar, demandsVar, capacity), Weak)
 
   val t0 = System.currentTimeMillis
+
   
-  for (t <- 0 until nTasks) {
-    if (t % 1000 == 0) println(t)
-    solver.doAndPropagate {
-      val variable = startsVar(t)
-      variable.assign(variable.min)
-    }
+  onSolution {
+    println("max=" + endsVar.map(_.value).max)
   }
   
+  search {
+    binaryStatic(startsVar, _.min)
+  }
+  val stat = start(nSols = 1)
+  println(stat)
   val time = System.currentTimeMillis - t0
 
-  println(time)
-  println("max=" + endsVar.map(_.value).max)
+  println("time:"+time)
+  
 }
