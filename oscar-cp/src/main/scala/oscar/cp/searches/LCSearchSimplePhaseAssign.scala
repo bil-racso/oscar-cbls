980f53f510c97f1e0f5ccb3dd4371643279b61e8
rhartert <devnull@localhost>
2015-02-05 15:42:50 +0100

    Minor changes

--- oscar-cp/src/main/scala/oscar/cp/searches/LCSearchSimplePhaseAssign.scala
+++ oscar-cp/src/main/scala/oscar/cp/searches/LCSearchSimplePhaseAssign.scala
@@ -27,108 +27,117 @@ import oscar.algo.search.Branching
  * Remember the last value tried on every variable in phase.
  * Use phase(i), which is the last value that did not fail on assign,
  * to guide the assignment/split chosen for branching.
- *  
+ *
  * @author Steven Gay
  * @author Renaud Hartert
  */
 
 class LCSearchSimplePhaseAssign(variables: Array[CPIntVar], varHeuris: Int => Int, valHeuris: Int => Int) extends Branching {
+
   require(variables.length > 0, "no variables")
-  
-  private final val nVariables = variables.length
-  private final val store = variables(0).store
-  
-  private final val order = Array.tabulate(nVariables){i => i}
-  private final val phase = Array.fill(nVariables)(0) // if variable was never visited, always use valHeuris
 
-  private final val depth = new ReversibleInt(store, 0)
+  private[this] val nVariables = variables.length
+  private[this] val store = variables(0).store
 
+  // Order in which variables have to be assigned
+  private[this] val order = Array.tabulate(nVariables) { i => i }
 
-  private var maxDepth: Int = -1
-  private var deepest: Int = 0
+  // Prefered value for each variable
+  private[this] val phase = Array.fill(nVariables)(0)
+
+  // Current depth of the search tree
+  private[this] val depth = new ReversibleInt(store, 0)
+
+  private[this] var maxDepth: Int = -1
+
+  private[this] var deepest: Int = 0
 
   final def reset(): Unit = maxDepth = -1
 
   final override def alternatives: Seq[Alternative] = {
-    // Update depth
-    var d = depth.value
-    while (d < nVariables && variables(order(d)).isBound) {
-      val dd = order(d)
-      phase(dd) = variables(dd).value
-      d += 1
-    }
-    depth.value = d
-    
-    if (d >= nVariables) return noAlternative
-    
-    // Use variable heuristic to find next variable to use
-    if (d > maxDepth) {
-      maxDepth = d
-      deepest = findBestHeuristic(d)
-      val dd = order(deepest)
-      phase(dd) = valHeuris(dd)
-    }
-    
-    // pull variable that last conflicted or was chosen by variable heuristic up at depth
-    if (deepest > d && !variables(order(deepest)).isBound) {
-      val deepestInd = order(deepest)
-      System.arraycopy(order, d, order, d + 1, deepest - d)
-      order(d) = deepestInd
-    }
-    
-    deepest = d
-    
-    val dd = order(d)
-    val x = variables(dd)
-    
-    val minX = x.min
-    val maxX = x.max
-    val p = phase(dd)
-    
-    
-    // if phase is in domain, use it to guide search
-    if (minX <= p && p <= maxX) {
-      // if domain is continuous, do assign/split left/split right
-      var s = Seq(() => { store.assign(x, p) ; if (!store.isFailed) phase(dd) = p })
-      
-      if (minX < p) {
-        s = s :+ (() => { store.post(x < p) ; if (!store.isFailed) phase(dd) = p - 1 })
-      }
-      
-      if (p < maxX) {
-        s = s :+ (() => { store.post(x > p) ; if (!store.isFailed) phase(dd) = p + 1 })
-      }
-      
-      s
-      // TODO: if domain not continuous, do assign/remove
-    }
-    // otherwise, use value heuristic
+    val d = updateDepth() // compute the current depth   
+    if (d >= nVariables) noAlternative
     else {
-      // always do assign/remove
-      val v = valHeuris(dd)
-      Seq(() => { store.assign(x, v)  ; if (!store.isFailed) phase(dd) = v },
+
+      // Trail the new depth
+      depth.value = d
+
+      // Use variable heuristic to find next variable to use
+      if (d > maxDepth) {
+        maxDepth = d
+        deepest = findBestHeuristic(d)
+        val varId = order(deepest)
+        phase(varId) = valHeuris(varId)
+      }
+
+      // pull variable that last conflicted or was chosen by variable heuristic up at depth
+      if (deepest > d && !variables(order(deepest)).isBound) {
+        val deepestInd = order(deepest)
+        System.arraycopy(order, d, order, d + 1, deepest - d) // Use a successor vector instead
+        order(d) = deepestInd
+      }
+
+      deepest = d
+
+      val dd = order(d)
+      val x = variables(dd)
+
+      val minX = x.min
+      val maxX = x.max
+      val p = phase(dd)
+
+      // if phase is in domain, use it to guide search
+      if (minX <= p && p <= maxX) {
+        // if domain is continuous, do assign/split left/split right
+        var s = Seq(() => { store.assign(x, p); if (!store.isFailed) phase(dd) = p })
+
+        if (minX < p) {
+          s = s :+ (() => { store.post(x < p); if (!store.isFailed) phase(dd) = p - 1 })
+        }
+
+        if (p < maxX) {
+          s = s :+ (() => { store.post(x > p); if (!store.isFailed) phase(dd) = p + 1 })
+        }
+
+        s
+        // TODO: if domain not continuous, do assign/remove
+      } // otherwise, use value heuristic
+      else {
+        // always do assign/remove
+        val v = valHeuris(dd)
+        Seq(() => { store.assign(x, v); if (!store.isFailed) phase(dd) = v },
           () => { store.remove(x, v) }
-          )
+        )
+      }
     }
   }
 
+  @inline private def updateDepth(): Int = {
+    var d = depth.value
+    while (d < nVariables && variables(order(d)).isBound) {
+      val varId = order(d)
+      phase(varId) = variables(varId).min
+      d += 1
+    }
+    d
+  }
 
-  def findBestHeuristic(depth: Int): Int = { depth }
-  
   // is this a Scala collection command?
-  def findBestHeuristic0(depth: Int): Int = {
+  def findBestHeuristic(depth: Int): Int = {
     var bestIndex = depth
     var bestScore = Int.MaxValue
     var p = depth
     while (p < nVariables) {
-      val pScore = varHeuris(order(p)) 
-      if (pScore < bestScore) {
-        bestScore = pScore
-        bestIndex = p
+      val varId = order(p)
+      if (!variables(varId).isBound) {
+        val pScore = varHeuris(varId)
+        if (pScore < bestScore) {
+          bestScore = pScore
+          bestIndex = p
+        }
       }
       p += 1
     }
     bestIndex
   }
-
 }
