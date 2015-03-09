eb240119ade6cad32147b34459eda9e6530210ec
Cyrille Dejemeppe <cyrille.dejemeppe@gmail.com>
2015-03-09 15:08:46 +0100

    Added Dijkstra's shortest path algorithm for Double edge costs

--- oscar-algo/src/main/scala/oscar/algo/graph/GraphUtils.scala
+++ oscar-algo/src/main/scala/oscar/algo/graph/GraphUtils.scala
@@ -67,4 +67,58 @@ object GraphUtils {
 
     Array.tabulate(n)(i => (getPath(i), distances(i)))
   }
+
+  /**
+   * This method computes the shortest path from a source to a sink
+   * with regards to the edgeCosts matrix using the famous Dijkstra
+   * algorithm (http://en.wikipedia.org/wiki/Dijkstra%27s_algorithm).
+   * These edge costs must be positive otherwise the
+   * Bellman–Ford–Moore algorithm must be used.
+   *
+   * @param source Index representing the source node
+   * @param edgeCosts Matrix of the size nodes x nodes where each entry
+   *                  (i)(j) represents the cost of the edge going from
+   *                  node i to node j. If there is no edge from i to j,
+   *                  then the entry must be Int.MaxValue.
+   * @return An array of pairs (path, cost) where, for index i, path is,
+   *         the succession of index of nodes in the shortest path from
+   *         the source to the node i;
+   *         cost is the sum of the edge costs along this shortest path
+   */
+  def dijkstra(source: Int, edgeCosts: Array[Array[Double]]): Array[(Array[Int], Int)] = {
+    val n = edgeCosts.length
+    val distances = Array.fill(n)(Int.MaxValue)
+    val previous = Array.fill(n)(UNDEF)
+    distances(source) = 0
+
+    val Q = new ArrayHeapInt(n)
+    for (i <- 0 until n) {
+      Q.enqueue(i, distances(i))
+    }
+
+    while(!Q.isEmpty) {
+      val u = Q.dequeue()
+
+      for (v <- 0 until n if u != v) {
+        val alt = distances(u) + edgeCosts(u)(v)
+        if (alt < distances(v)) {
+          Q.changeKey(distances(v), alt, v)
+          distances(v) = alt
+          previous(v) = u
+        }
+      }
+    }
+
+    def getPath(endPoint: Int): Array[Int] = {
+      var path = List[Int]()
+      var i = 0
+      while(i != source) {
+        path +:= i
+        i = previous(i)
+      }
+      path.reverse.toArray
+    }
+
+    Array.tabulate(n)(i => (getPath(i), distances(i)))
+  }
 }
