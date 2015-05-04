a7214bbcb8f0248f76dcbb30379acd1ff2440305
Yoann Guyot <yoann.guyot@cetic.be>
2015-05-04 10:47:01 +0200

    Used IntITE in setEndWindow, to let one able to choose zero as the end window value. Added a function for adding a constraint on vehicle end time. Added requirements in travel time functions.

--- oscar-cbls/src/main/scala/oscar/cbls/routing/model/TravelTimeFunction.scala
+++ oscar-cbls/src/main/scala/oscar/cbls/routing/model/TravelTimeFunction.scala
@@ -14,23 +14,30 @@
  * If not, see http://www.gnu.org/licenses/lgpl-3.0.en.html
  * ****************************************************************************
  */
-/*******************************************************************************
-  * Contributors:
-  *     This code has been initially developed by Renaud De Landtsheer
-  ******************************************************************************/
+/**
+ * *****************************************************************************
+ * Contributors:
+ *     This code has been initially developed by Renaud De Landtsheer
+ * ****************************************************************************
+ */
 
 package oscar.cbls.routing.model
 
-import oscar.cbls.constraints.lib.basic.{GE, LE}
-import oscar.cbls.invariants.core.computation.{CBLSIntConst, CBLSIntVar, IntValue}
+import oscar.cbls.constraints.lib.basic.GE
+import oscar.cbls.constraints.lib.basic.LE
+import oscar.cbls.invariants.core.computation.CBLSIntConst
+import oscar.cbls.invariants.core.computation.CBLSIntVar
+import oscar.cbls.invariants.core.computation.IntValue
 import oscar.cbls.invariants.lib.logic.IntInt2Int
 import oscar.cbls.invariants.lib.minmax.Max2
 import oscar.cbls.invariants.lib.numeric.Sum
 import oscar.cbls.modeling.Algebra._
+import oscar.cbls.invariants.lib.logic.IntITE
 
-/** an abstract class representing a travel time function
-  * @author renaud.delandtsheer@cetic.be
-  */
+/**
+ * an abstract class representing a travel time function
+ * @author renaud.delandtsheer@cetic.be
+ */
 abstract class TravelTimeFunction {
   def getTravelDuration(from: Int, leaveTime: Int, to: Int): Int
   def getBackwardTravelDuration(from: Int, arrivalTime: Int, to: Int): Int
@@ -42,7 +49,8 @@ abstract class TravelTimeFunction {
   def getMaxTravelDuration(from: Int, to: Int): Int
 }
 
-/** adds the notion of time to your VRP
+/**
+ * adds the notion of time to your VRP
  * @author renaud.delandtsheer@cetic.be
  */
 trait Time extends VRP with Predecessors {
@@ -98,13 +106,20 @@ trait TravelTimeAsFunction extends VRP with Time {
   }
 }
 
-/** to post time window constraints
+/**
+ * to post time window constraints
  * @author renaud.delandtsheer@cetic.be
  */
 trait TimeWindow extends Time with StrongConstraints {
 
   def setEndWindow(node: Int, endWindow: Int) {
-    strongConstraints.post(LE(leaveTime(node), endWindow))
+    require(node >= V, "only for specifying time windows on nodes, not on vehicles")
+    strongConstraints.post(LE(IntITE(next(node), 0, leaveTime(node), N-1), endWindow))
+  }
+
+  def setVehicleEnd(vehicle: Int, endTime: Int) {
+    require(vehicle < V, "only for specifying end time of vehicles")
+    strongConstraints.post(LE(arrivalTime(vehicle), endTime))
   }
 
   def setNodeDuration(node: Int, durationWithoutWait: CBLSIntVar, startWindow: Int) {
@@ -118,15 +133,16 @@ trait TimeWindow extends Time with StrongConstraints {
 
 }
 
-/** addition ot the [[oscar.cbls.routing.model.TimeWindow]] trait, adds a variable representing the waiting duration
-  * @author renaud.delandtsheer@cetic.be
-  */
+/**
+ * addition ot the [[oscar.cbls.routing.model.TimeWindow]] trait, adds a variable representing the waiting duration
+ * @author renaud.delandtsheer@cetic.be
+ */
 trait WaitingDuration extends TimeWindow {
   val waitingDuration = Array.tabulate(N) {
     (i: Int) => CBLSIntVar(m, 0, 0 to Int.MaxValue / N, "WaitingDurationBefore" + i)
   }
 
-  def setNodeDurationAndWaitingTime(node: Int, durationWithoutWait: CBLSIntVar, waitingDuration:CBLSIntVar) {
+  def setNodeDurationAndWaitingTime(node: Int, durationWithoutWait: CBLSIntVar, waitingDuration: CBLSIntVar) {
     super.setNodeDuration(node, durationWithoutWait + waitingDuration)
     this.waitingDuration(node) <== waitingDuration
   }
