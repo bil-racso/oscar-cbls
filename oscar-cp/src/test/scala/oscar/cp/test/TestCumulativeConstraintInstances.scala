545646a8112ff1fe43b626db02b0b9548f0dbddb
StevenGay <devnull@localhost>
2015-06-09 09:45:53 +0200

    Generalized cumulative test, not finished

---
+++ oscar-cp/src/test/scala/oscar/cp/test/TestCumulativeConstraintInstances.scala
@@ -0,0 +1,131 @@
+package oscar.cp.test
+
+
+import oscar.cp.core.Constraint
+import oscar.cp.constraints.SweepMaxCumulative
+import oscar.cp._
+import oscar.cp.testUtils._
+import oscar.cp.constraints.CumulativeDecomp
+import oscar.cp.searches.BinaryStaticOrderBranching
+import oscar.cp.constraints.EnergeticReasoning
+import oscar.cp.core.CPPropagStrength
+
+
+import oscar.cp.core.CPOutcome._
+
+
+/*
+ *   Generates instances for cumulative constraints,
+ *   filter them with given cumulative stack,
+ *   and check that filtered values are actually never part of a solution.
+ *   This allows to catch situations where a cumulative propagator has removed a value that it should not have. 
+ */
+/*
+abstract class TestCumulativeConstraintInstances(val cumulativeName: String, val nTests: Int = 1000, val minDuration: Int = 0, val k: Int = 5) extends TestSuite {
+  type Ints = Array[Int]
+  
+  class SchedulingInstance(
+      val smin: 
+      ) {
+    implicit val solver = new CPStore()
+  }
+  
+  case class SchedulingInstance(val durations: Array[Int], val demands: Array[Int], val resources: Array[Int], val capacity: Array[Int], val horizon: Int) {
+    override def toString: String = {
+      val dur = "durations: " + durations.mkString(", ")
+      val dem = "demands: " + demands.mkString(", ")
+      val res = "resources: " + resources.mkString(", ")
+      val cap = "capacity: " + capacity.mkString(", ")
+      val hor = "horizon: " + horizon
+      dur + "\n" + dem + "\n" + res + "\n" + cap + "\n" + hor
+    }
+  }
+
+  class CPSched(instance: SchedulingInstance) extends CPSolver {
+    implicit val solver = this
+    silent = true
+    val nTasks = instance.demands.size
+    val Tasks = 0 until nTasks
+    val nResources = instance.capacity.size
+    val Resources = 0 until nResources
+    val durations = Array.tabulate(nTasks)(t => CPIntVar(instance.durations(t)))
+    val starts    = Array.tabulate(nTasks)(t => CPIntVar(0 to instance.horizon - durations(t).min))
+    val ends      = Array.tabulate(nTasks)(t => CPIntVar(durations(t).min to instance.horizon))
+    val demands   = Array.tabulate(nTasks)(t => CPIntVar(instance.demands(t)))
+    val resources = Array.tabulate(nTasks)(t => CPIntVar(instance.resources(t)))
+    Tasks.foreach(t => post(ends(t) == starts(t) + durations(t)))
+  }
+
+  def cumulative(starts: Array[CPIntVar], durations: Array[CPIntVar], ends: Array[CPIntVar], demands: Array[CPIntVar], resources: Array[CPIntVar], capacity: CPIntVar, id: Int): Array[Constraint]
+
+  def generateRandomSchedulingProblem(nTasks: Int): SchedulingInstance = {
+    val rand = new scala.util.Random()
+    val durations = Array.fill(nTasks)(rand.nextInt(4) + minDuration)
+    val demands = Array.fill(nTasks)(rand.nextInt(4))
+    val capacity = Array(rand.nextInt(4) + 1)
+    val resources = Array.fill(nTasks)(0)
+    val horizon = rand.nextInt(durations.sum + 1 - durations.max) + durations.max
+    SchedulingInstance(durations, demands, resources, capacity, horizon)
+  }
+
+  def solveAll(cp: CPSched, capacity: Array[Int], decomp: Boolean, cons: CPPropagStrength): Set[Sol] = {
+    if (!decomp) cp.Resources.foreach(r => cp.post(cumulative(cp.starts, cp.durations, cp.ends, cp.demands, cp.resources, CPIntVar(capacity(r))(cp), r),cons))
+    else cp.Resources.foreach(r => cp.post(new CumulativeDecomp(cp.starts, cp.durations, cp.ends, cp.demands, cp.resources, CPIntVar(capacity(r))(cp), r)))
+    var sols: List[Sol] = List()
+    
+    cp.post(cp.starts(0)+2 >= cp.starts(4))
+    cp.post(cp.starts(4) >= cp.ends(3)-1)
+    
+
+    cp.search {
+      val b1 = binaryStatic(cp.starts,_.randomValue)
+      val b2 = binaryStatic(cp.durations,_.randomValue)
+      val b3 = binaryStatic(cp.resources)
+      b1 ++ b2 ++ b3
+    }
+
+    cp.onSolution {
+      val sol = cp.starts.map(_.value).toList
+      sols = sol :: sols
+    }
+
+    cp.start()
+
+    sols.toSet
+  }
+
+  def compare(sols1: Set[Sol], sols2: Set[Sol]): Boolean = {
+    if (sols1.size != sols2.size) false
+    else sols1.forall(s => sols2 contains s)
+  }
+
+  test("test solveAll " + cumulativeName) {
+    for (i <- 1 to nTests) {
+      //print("test " + cumulativeName + " instance " + i + ": ")
+      val instance = generateRandomSchedulingProblem(k)
+      val cpDecomp = new CPSched(instance)
+      val allSolsDecomp = solveAll(cpDecomp, instance.capacity, true, CPPropagStrength.Weak)
+      //println(allSolsDecomp.size)
+
+      for (cons <- Array(CPPropagStrength.Weak)) {
+        val cpCumul = new CPSched(instance)
+        val allSolsCumul = solveAll(cpCumul, instance.capacity, false, cons)
+
+        val ok = compare(allSolsDecomp, allSolsCumul)
+        if (!ok) {
+          print("test " + cumulativeName + " instance " + i + ": ")
+          println("failed ! with consistency " + cons)
+          println("expected number of solutions: " + allSolsDecomp.size)
+          println("number of solutions: " + allSolsCumul.size)
+          println("INSTANCE")
+          println(instance)
+
+          false
+        }
+        ok should be(true)
+      }
+    }
+  }
+}
+*
+*/
