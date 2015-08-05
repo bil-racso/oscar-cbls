119b80e8d0408e43128ccce8c08d7199c5e9ed39
vlecomte <vlecomte@u310>
2015-08-05 23:03:37 +0200

    Returns Success more often; more optimized; cooler code

--- oscar-cp/src/main/scala/oscar/cp/constraints/GCCFWC.scala
+++ oscar-cp/src/main/scala/oscar/cp/constraints/GCCFWC.scala
@@ -39,202 +39,199 @@ class GCCFWC(val X: Array[CPIntVar], val minVal: Int, val lower: Array[Int], val
   idempotent = false
 
   val nValues = lower.length
+  val nSubconstraints = 2 * nValues
   val rValues = minVal until (minVal + nValues)
-  val valuesIdx = 0 until nValues
 
   // MEMORIZATION STRUCTURE:
-  // Number of variables currently bound to the value
-  val nBound = Array.tabulate(nValues)(_ => new ReversibleInt(s, 0))
-  // Number of unbound variables currently having the value
-  val nUnbound = Array.tabulate(nValues)(_ => new ReversibleInt(s, 0))
-  // List of the unbound variables currently having the value
-  val unboundForValue = Array.tabulate(nValues)(_ => new ReversibleSparseSet(s, 0, X.length - 1))
-  // Whether the condition on the value is definitely respected or not
-  val definitelyOk = Array.tabulate(nValues)(_ => new ReversibleBoolean(s, false))
-  // The number of values whose conditions are definitely respected
-  val nDefinitelyOk = new ReversibleInt(s, 0)
-
+  // The number of variables that *must* take this value
+  var nMandatoryRev: Array[ReversibleInt] = null
+  // The number of variables that *may* take this value
+  var nPossibleRev: Array[ReversibleInt] = null
+  // The number of subconstraints among the 2*nValues subconstraints that are respected
+  var nDefinitelyOkRev: ReversibleInt = null
 
   // Change buffer to load the deltas
   var changeBuffer: Array[Int] = null
-  // Buffer to load unbound variables from `unboundForValue`
-  val unboundBuffer = new Array[Int](X.length)
 
   override def setup(l: CPPropagStrength): CPOutcome = {
 
+    // Temporary variables to avoid using reversible variables too much
+    val nMandatory = new Array[Int](nValues)
+    val nPossible = new Array[Int](nValues)
+    var nDefinitelyOk = 0
+
     // Initial counting (the rest is done in the update functions)
-    for ((x, i) <- X.zipWithIndex) {
-      if (x.isBound && rValues.contains(x.min)) {
-        nBound(x.min - minVal).incr()
-      }
-      for (vi <- valuesIdx) {
-        if (x.hasValue(vi + minVal) && !x.isBound) {
-          nUnbound(vi).incr()
-        } else {
-          unboundForValue(vi).removeValue(i)
+    var bufferSize = 0
+    var i = X.length
+    while (i > 0) {
+      i -= 1
+      val x = X(i)
+
+      if (x.isBound) {
+        val v = x.min
+        if (rValues.contains(v)) {
+          val vi = v - minVal
+          nMandatory(vi) += 1
         }
       }
-    }
+      var vi = nValues
+      while (vi > 0) {
+        vi -= 1
+        if (x.hasValue(vi + minVal)) {
+          nPossible(vi) += 1
+        }
+      }
 
-    // Initializing change buffer
-    var bufferSize = 0
-    for (x <- X) {
-      bufferSize = bufferSize max x.size
-    }
-    changeBuffer = new Array(bufferSize)
+      // Adapt the change buffer size
+      if (bufferSize < x.size) {
+        bufferSize = x.size
+      }
 
-    // We want to get notifications for the crap we do during the first check loop
-    for ((x, i) <- X.zipWithIndex) {
-      x.callOnChanges(i, whenDomainChanges(_, x, i))
+      // Register before the first check loop so that we receive information on what we changed there
+      x.callOnChanges(i, whenDomainChanges(_, x))
     }
 
     // First check loop (according to the counts in the initial counting)
-    for (vi <- valuesIdx) {
+    var vi = nValues
+    while (vi > 0) {
+      vi -= 1
 
+      // Few enough variables have the value
+      if (nPossible(vi) <= upper(vi)) {
+        nDefinitelyOk += 1
+      }
       // Too few variables have the value
-      if (nBound(vi).value + nUnbound(vi).value < lower(vi)) {
+      if (nPossible(vi) < lower(vi) ||
+        (nPossible(vi) == lower(vi) && atLowerBound(vi) == Failure)) {
         return Failure
+      }
+
+      // Enough variables are bound to the value
+      if (nMandatory(vi) >= lower(vi)) {
+        nDefinitelyOk += 1
       }
       // Too many variables are bound to the value
-      if (nBound(vi).value > upper(vi)) {
+      if (nMandatory(vi) > upper(vi) ||
+        (nMandatory(vi) == upper(vi) && atUpperBound(vi) == Failure)) {
         return Failure
       }
-
-      // Check for the corresponding equality cases
-      if (onTotalDecrease(vi) == Failure) {
-        return Failure
-      }
-      if (onBoundIncrease(vi) == Failure) {
-        return Failure
-      }
-
-      updateOk(vi)
+    }
+    
+    // If the constraint is already respected
+    if (nDefinitelyOk == nSubconstraints) {
+      return Success
     }
 
-    status()
+    // Initialize the memorization structure
+    nMandatoryRev = Array.tabulate(nValues)(_ => new ReversibleInt(s, 0))
+    nPossibleRev = Array.tabulate(nValues)(_ => new ReversibleInt(s, 0))
+    nDefinitelyOkRev = new ReversibleInt(s, 0)
+
+    // Create the buffer
+    changeBuffer = new Array(bufferSize)
+
+    // Update the structure
+    vi = nValues
+    while (vi > 0) {
+      vi -= 1
+      nMandatoryRev(vi).setValue(nMandatory(vi))
+      nPossibleRev(vi).setValue(nPossible(vi))
+    }
+    nDefinitelyOkRev.setValue(nDefinitelyOk)
+
+    Suspend
   }
 
   /**
    * Update the structure when values are removed from a variable
    */
-  def whenDomainChanges(delta: DeltaIntVar, x: CPIntVar, i: Int): CPOutcome = {
+  @inline private def whenDomainChanges(delta: DeltaIntVar, x: CPIntVar): CPOutcome = {
+    val i = delta.id
     var c = delta.fillArray(changeBuffer)
     while (c > 0) {
       c -= 1
-      if (rValues.contains(changeBuffer(c))) {
-        val vi = changeBuffer(c) - minVal
-        nUnbound(vi).decr()
-        unboundForValue(vi).removeValue(i)
+      val v = changeBuffer(c)
 
-        if (onTotalDecrease(vi) == Failure) {
+      // For every relevant value removed from the variable
+      if (rValues.contains(v)) {
+        val vi = v - minVal
+        val nPossible = nPossibleRev(vi).decr()
+
+        if (nPossible == upper(vi) && nDefinitelyOkRev.incr() == nSubconstraints) {
+          return Success
+        }
+        if (nPossible == lower(vi) && atLowerBound(vi) == Failure) {
           return Failure
         }
-        updateOk(vi)
       }
     }
     if (x.isBound) {
-      whenBind(x, i)
+      whenBind(x.min, i)
     } else {
-      status()
+      Suspend
     }
   }
 
   /**
    * Update the structure when a variable is bound
    */
-  def whenBind(x: CPIntVar, i: Int): CPOutcome = {
-    if (rValues.contains(x.min)) {
-      val vi = x.min - minVal
-      nUnbound(vi).decr()
-      nBound(vi).incr()
-      unboundForValue(vi).removeValue(i)
+  @inline private def whenBind(v: Int, i: Int): CPOutcome = {
+    if (rValues.contains(v)) {
+      val vi = v - minVal
+      val nMandatory = nMandatoryRev(vi).incr()
 
-      if (onBoundIncrease(vi) == Failure) {
+      if (nMandatory == lower(vi) && nDefinitelyOkRev.incr() == nSubconstraints) {
+        return Success
+      }
+      if (nMandatory == upper(vi) && atUpperBound(vi) == Failure) {
         return Failure
       }
-      updateOk(vi)
-    }
-    status()
-  }
-
-  /**
-   * When the number of variables having a value decreases, it might have reached the lower bound.
-   * In that case, one has to bind all the variables which have that value and were yet unbound.
-   * It may fail in case the value was actually removed from one of those variables in the meantime.
-   */
-  private def onTotalDecrease(vi: Int): CPOutcome = {
-
-    if (nBound(vi).value + nUnbound(vi).value == lower(vi)) {
-      val v = vi + minVal
-      var c = unboundForValue(vi).fillArray(unboundBuffer)
-      while (c > 0) {
-        c -= 1
-        if (X(unboundBuffer(c)).assign(v) == Failure) {
-          return Failure
-        }
-      }
     }
     Suspend
   }
 
   /**
-   * When the number of variables bound to a value increases, it might have reached the upper bound.
-   * In that case, one has to remove the value from any additional (unbound) variables.
-   * It may fail in case one of those variables was actually bound to the value in the meantime.
+   * Changes to be made when the number of possible variables reaches the lower bound.
    */
-  private def onBoundIncrease(vi: Int): CPOutcome = {
+  @inline private def atLowerBound(vi: Int): CPOutcome = {
+    val v = vi + minVal
+    var nPossible = 0
 
-    if (nBound(vi).value == upper(vi)) {
-      val v = vi + minVal
-      var c = unboundForValue(vi).fillArray(unboundBuffer)
-      while (c > 0) {
-        c -= 1
-        if (X(unboundBuffer(c)).removeValue(v) == Failure) {
-          return Failure
+    var i = X.length
+    while (i > 0) {
+      i -= 1
+      val x = X(i)
+      if (x.hasValue(v)) {
+        if (!x.isBound) {
+          x.assign(v)
         }
+        nPossible += 1
       }
     }
-    Suspend
+
+    if (nPossible == lower(vi)) Suspend
+    else Failure
   }
 
   /**
-   * Update the success status for a value in the range
+   * Changes to be made when the number of mandatory variables reaches the upper bound
    */
-  private def updateOk(vi: Int) = {
+  @inline private def atUpperBound(vi: Int): CPOutcome = {
+    val v = vi + minVal
+    var nMandatory = 0
 
-    if (!definitelyOk(vi).value &&
-      nBound(vi).value >= lower(vi) &&
-      nBound(vi).value + nUnbound(vi).value <= upper(vi)) {
-
-      definitelyOk(vi).setValue(true)
-      nDefinitelyOk.incr()
+    var i = X.length
+    while (i > 0) {
+      i -= 1
+      val x = X(i)
+      if (x.isBoundTo(v)) {
+        nMandatory += 1
+      } else if (x.hasValue(v)) {
+        x.removeValue(v)
+      }
     }
-  }
 
-  /**
-   * Compute current outcome
-   */
-  private def status() =
-    if (nDefinitelyOk.value == nValues) Success
-    else Suspend
-
-  def debug() {
-    println("General state:")
-    println("Variables:")
-    for (x <- X) println(x.toArray.mkString(" "))
-    println("nBound:")
-    println(nBound.map(_.value).mkString(" "))
-    println("nUnbound:")
-    println(nUnbound.map(_.value).mkString(" "))
-    println("unboundForValue:")
-    for (u <- unboundForValue) {
-      val it = u.iterator
-      while (it.hasNext) print(it.next() + " ")
-      println()
-    }
-    println("currentlyOk:")
-    println(definitelyOk.map(_.value).mkString(" "))
-    println("nCurrentlyOk: " + nDefinitelyOk.value)
-    println()
+    if (nMandatory == upper(vi)) Suspend
+    else Failure
   }
 }
