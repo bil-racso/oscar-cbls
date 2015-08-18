b825c39b2b9d042f596dcbe56c93aaba005b259a
Renaud De Landtsheer <renaud.delandtsheer@cetic.be>
2015-08-18 15:01:49 +0200

    updating listeners

--- oscar-des/src/main/scala/oscar/des/flow/lib/Listeners.scala
+++ oscar-des/src/main/scala/oscar/des/flow/lib/Listeners.scala
@@ -3,17 +3,16 @@ package oscar.des.flow.lib
 //This file is about thing we want to measure on the factory process
 
 //Variables have values evey time something is happening.
-class Variable[T]
-class BoolExpr extends Variable[Boolean]
-class IntExpr extends Variable[Int]
-class IntConstant() extends Variable[Int]
-class TruthValue() extends Variable[Boolean]
+class BoolExpr
+class IntExpr
 
 //probe on simulation elements
 class Empty(s:Storage) extends BoolExpr
 class Productive(p:Process) extends BoolExpr
+class Content(s:Storage) extends IntExpr
 
 //logical properties
+//boolean is whet it means: a boolean value at each state. there is no notion of event there; they are like fluents.
 //we only consider temporal operators of the past, easy to evaluate
 class Not(f:BoolExpr) extends BoolExpr
 class And(f:BoolExpr, g:BoolExpr) extends BoolExpr
@@ -22,12 +21,8 @@ class Or(f:BoolExpr, g:BoolExpr) extends BoolExpr
 class HasAlwaysBeen(f:BoolExpr) extends BoolExpr
 class HasBeen(f:BoolExpr) extends BoolExpr
 class Since(a:BoolExpr,b:BoolExpr) extends BoolExpr  //the counterpart is only differing with its initial value
-
-//selecting some events (thus value transitions on fluents)
-//events happen at event time; besides, they have no interpretation.
-class Event
-class BT(p:BoolExpr) extends BoolExpr
-class BF(p:BoolExpr) extends BoolExpr
+class BecomesTrue(p:BoolExpr) extends BoolExpr
+class BecomesFalse(p:BoolExpr) extends BoolExpr
 class Changes(p:BoolExpr) extends BoolExpr
 
 //variables always have a value.
@@ -51,9 +46,3 @@ class Statistics
 //this only considers the latest valuee of e; at the end of the simulation run, and performs an average over several runs
 class Mean(e:IntExpr) extends Statistics
 class Variance(e:IntExpr) extends Statistics
-
-
-/*class Model(){
-  val durationSinceLastTick:Entier
-  val currentTime:Entier
-}*/
