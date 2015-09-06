e48ff4e9fcf21f1c0c50d68a973b4d3687a9e7ba
pschaus <devnull@localhost>
2015-09-06 08:34:49 +0200

    modif max cumulative: weak = only timetable

--- oscar-cp/src/main/scala/oscar/cp/scheduling/constraints/MaxCumulative.scala
+++ oscar-cp/src/main/scala/oscar/cp/scheduling/constraints/MaxCumulative.scala
@@ -9,25 +9,24 @@ import oscar.cp.core.CPPropagStrength._
 import oscar.cp.scheduling.constraints._
 
 class MaxCumulative(starts: Array[CPIntVar], durations: Array[CPIntVar], ends: Array[CPIntVar], demands: Array[CPIntVar], resources: Array[CPIntVar], capacity: CPIntVar, id: Int = 1)
-extends Constraint(starts.head.store, "Max Cumulative") {
+  extends Constraint(starts.head.store, "Max Cumulative") {
   override def setup(l: CPPropagStrength): CPOutcome = {
-     if (s.post(new MaxCumulativeCapaCheck(starts,durations,ends,demands,resources,capacity,id)) == Failure) {
-       return Failure
-     }
+    if (s.post(new MaxCumulativeCapaCheck(starts,durations,ends,demands,resources,capacity,id)) == Failure) {
+      return Failure
+    }
 
     l match {
-      case Automatic | Weak => 
+      case Weak =>
+        if (s.post(TTPerTask(starts,durations,ends,demands,resources,capacity,id)) == Failure) return Failure
+      // steven, stop adding things here, weak = time-table only
+      case Automatic =>
         if (s.post(TTPerTask(starts,durations,ends,demands,resources,capacity,id)) == Failure) return Failure
         if (s.post(TimeTableOverloadChecker(starts,durations,ends,demands,resources,capacity,id)) == Failure) return Failure
-        
-        
       case Medium =>
         if (s.post(TTPerTask(starts,durations,ends,demands,resources,capacity,id)) == Failure) return Failure
         if (s.post(TimeTableDisjunctiveReasoning(starts,durations,ends,demands,resources,capacity,id)) == Failure) return Failure
         if (s.post(TimeTableOverloadChecker(starts,durations,ends,demands,resources,capacity,id)) == Failure) return Failure
         if (s.post(TimeTableEdgeFinding(starts,durations,ends,demands,resources,capacity,id)) == Failure) return Failure
-        
-        
       case Strong =>
         if (s.post(TTPerTask(starts,durations,ends,demands,resources,capacity,id)) == Failure) return Failure
         if (s.post(TimeTableDisjunctiveReasoning(starts,durations,ends,demands,resources,capacity,id)) == Failure) return Failure
@@ -35,7 +34,7 @@ extends Constraint(starts.head.store, "Max Cumulative") {
         if (s.post(TimeTableEdgeFinding(starts,durations,ends,demands,resources,capacity,id)) == Failure) return Failure
         if (s.post(EnergeticReasoning(starts,durations,ends,demands,resources,capacity,id)) == Failure) return Failure
     }
-    
+
     Success
   }
 }
