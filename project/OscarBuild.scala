c16cd93699e2da3212ae345cdd594ec085133f70
acrucifix <acr@n-side.com>
2016-05-01 18:27:58 +0200

    updating nside dev version

--- project/OscarBuild.scala
+++ project/OscarBuild.scala
@@ -11,7 +11,7 @@ object OscarBuild extends Build {
 
   object BuildSettings {
     val buildOrganization = "oscar"
-    val buildVersion = "3.1.1.3-NSIDE"
+    val buildVersion = "3.1.1.4-NSIDE"
     val buildScalaVersion = "2.11.0"
     val buildSbtVersion= "0.13.0"
 
