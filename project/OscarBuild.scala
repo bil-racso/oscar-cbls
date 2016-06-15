1b36bbd135818422b6095f6518d759e29dba0204
nside <nside@gurobi-test-instance>
2016-06-15 14:26:43 +0200

    adding java library path from the system

--- project/OscarBuild.scala
+++ project/OscarBuild.scala
@@ -15,11 +15,11 @@ object OscarBuild extends Build {
     val buildScalaVersion = "2.11.0"
     val buildSbtVersion= "0.13.0"
 
-    val osNativeLibDir = (sys.props("os.name"), sys.props("os.arch")) match {
-      case (os, arch) if os.contains("Mac") && arch.endsWith("64") => "macos64"
-      case (os, arch) if os.contains("Linux") && arch.endsWith("64") => "linux64"
-      case (os, arch) if os.contains("Windows") && arch.endsWith("32") => "windows32"
-      case (os, arch) if os.contains("Windows") && arch.endsWith("64") => "windows64"
+    val (osNativeLibDir, osJavaLibraryPath) = (sys.props("os.name"), sys.props("os.arch")) match {
+      case (os, arch) if os.contains("Mac") && arch.endsWith("64") => ("macos64", sys.env("DYLD_LIBRARY_PATH")) 
+      case (os, arch) if os.contains("Linux") && arch.endsWith("64") => ("linux64", sys.env("LD_LIBRARY_PATH"))
+      case (os, arch) if os.contains("Windows") && arch.endsWith("32") => ("windows32", sys.env("PATH"))
+      case (os, arch) if os.contains("Windows") && arch.endsWith("64") => ("windows64", sys.env("PATH"))
       case (os, arch) => sys.error("Unsupported OS [${os}] Architecture [${arch}] combo, OscaR currently supports macos64, linux64, windows32, windows64")
     }
 
@@ -32,7 +32,7 @@ object OscarBuild extends Build {
         t => Tests.Argument(TestFrameworks.ScalaTest, "junitxml(directory=\"%s\")" format (t / "test-reports") ) },
       parallelExecution in Test := false,
       fork in Test := true,
-      javaOptions in Test += "-Djava.library.path=../lib:../lib/" + osNativeLibDir,
+      javaOptions in Test += "-Djava.library.path=../lib:../lib/" + osNativeLibDir + ":" + osJavaLibraryPath,
       scalaVersion := buildScalaVersion,
       unmanagedSourceDirectories in Test += baseDirectory.value / "src" / "main" / "examples",
       publishTo := {
