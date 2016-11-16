8c119f2fdfe5d0447eac3e483f1bde15599faf40
Guillaume Derval <guillaume.derval@uclouvain.be>
2016-11-16 15:38:23 +0100

    Add an helper to convert automagically CP constraints to Modeling constraints

---
+++ oscar-modeling/src/main/scala/oscar/modeling/constraints/ConvertCPConstraint.scala
@@ -0,0 +1,83 @@
+package oscar.modeling.constraints
+
+import oscar.algo.search.Outcome
+import oscar.cp
+import oscar.cp.CPSolver
+import oscar.cp.constraints.Automaton
+import oscar.modeling.vars.cp.CPIntVar
+import oscar.modeling.vars.{BoolVar, IntVar}
+
+/**
+  * Created by dervalguillaume on 16/11/16.
+  */
+object ConvertCPConstraint {
+  /**
+    * Convert an oscar.cp constraint to an oscar.modeling constraint
+    * @param args the arguments to the constructor of the class
+    */
+  def apply[T <: oscar.cp.Constraint](args: Any*)(implicit m : Manifest[T]): CPInstantiableConstraint = {
+    new CPInstantiableConstraint {
+      override def cpPost(cpSolver: CPSolver): Outcome = cpSolver.post(getConstraint[T](args: _*))
+    }
+  }
+
+  /**
+    * Convert args from oscar.modeling and returns a oscar.cp.Constraint
+    */
+  def getConstraint[T <: oscar.cp.Constraint](args: Any*)(implicit m : Manifest[T]): T = {
+    val constructors = m.runtimeClass.getConstructors
+    var i = 0
+    while(i != constructors.length) {
+      try {
+        val constructor = constructors(i)
+        val parameters = constructor.getParameterTypes
+        val realArgs = args.map(convertArg).zip(parameters).map(x => boxIfNeeded(x._2, x._1))
+        return constructor.newInstance(realArgs: _*).asInstanceOf[T]
+      }
+      catch {
+        case _: Throwable => i+=1 //could not instantiate this constructor, let's try another one
+      }
+    }
+    throw new RuntimeException("No valid constructor found")
+  }
+
+  /**
+    * Convert various types of arguments containing IntVar to the same type containing cp.CPIntVar
+    */
+  private def convertArg(arg: Any): Any = arg match {
+    case intvar: IntVar => intvar.getRepresentative.asInstanceOf[CPIntVar].realCPVar
+    case boolvar: BoolVar => boolvar.getRepresentative.asInstanceOf[CPIntVar].realCPVar.asInstanceOf[cp.CPBoolVar]
+    case int: Int => int
+    case long: Long => long
+    case double: Double => double
+    case bool: Boolean => bool
+    case char: Char => char
+    case string: String => string
+    case automaton: Automaton => automaton
+    case set: Set[_] => set.map(convertArg)
+    case seq: Seq[_] => seq.map(convertArg)
+    case list: List[_] => list.map(convertArg)
+    case array: Array[Int] => array
+    case array: Array[Long] => array
+    case array: Array[Double] => array
+    case array: Array[Boolean] => array
+    case array: Array[Char] => array
+    case array: Array[String] => array
+    case array: Array[IntVar] => array.map(convertArg(_).asInstanceOf[cp.CPIntVar])
+    case array: Array[BoolVar] => array.map(convertArg(_).asInstanceOf[cp.CPBoolVar])
+    case _ => throw new RuntimeException("Unknown parameter type "+arg.getClass.toString)
+  }
+
+  /**
+    * Box primivite classes. Needed because http://bugs.java.com/bugdatabase/view_bug.do?bug_id=6456930
+    */
+  private def boxIfNeeded(c: Class[_], v: Any): Object = c match {
+    case java.lang.Integer.TYPE => new java.lang.Integer(v.asInstanceOf[Int])
+    case java.lang.Float.TYPE => new java.lang.Float(v.asInstanceOf[Double])
+    case java.lang.Double.TYPE => new java.lang.Double(v.asInstanceOf[Double])
+    case java.lang.Long.TYPE => new java.lang.Long(v.asInstanceOf[Long])
+    case java.lang.Character.TYPE => new java.lang.Character(v.asInstanceOf[Char])
+    case java.lang.Boolean.TYPE => new java.lang.Boolean(v.asInstanceOf[Boolean])
+    case _ => c.cast(v).asInstanceOf[Object]
+  }
+}
---
+++ oscar-modeling/src/test/scala/oscar/modeling/test/TestCPConstraintConversion.scala
@@ -0,0 +1,62 @@
+package oscar.modeling.test
+import oscar.algo.search.Outcome
+import oscar.cp.{CPBoolVar, CPIntVar}
+import oscar.cp.core.CPPropagStrength
+import oscar.modeling.constraints.ConvertCPConstraint
+import oscar.modeling.models.{ModelDeclaration, UninstantiatedModel}
+import oscar.modeling.models.operators.CPInstantiate
+import oscar.modeling.testUtils.TestSuite
+import oscar.modeling.vars.{BoolVar, IntVar}
+
+abstract class MyTestConstraints extends oscar.cp.Constraint(null) {
+  def print: String
+  override def setup(l: CPPropagStrength): Outcome = Outcome.Success
+}
+
+final class StdTypeTest(a: Int, b: Long, c: String, d: Double, e: Boolean, f: Float) extends MyTestConstraints {
+  override def print: String = (a,b,c,d,e,f).toString()
+}
+
+final class StdCollectionsTest(a: Array[Int], b: Set[Long], c: Seq[Boolean]) extends MyTestConstraints {
+  override def print: String = (a.map(_.toString).mkString("-"), b.map(_.toString).mkString("-"), c.map(_.toString).mkString("-")).toString()
+}
+
+final class IntVarTest(a: CPIntVar, b: CPBoolVar) extends MyTestConstraints {
+  override def print: String = (a.min, b.min).toString()
+}
+
+final class ArrayTest(a: Array[CPIntVar], b: Seq[CPBoolVar]) extends MyTestConstraints {
+  override def print: String = (a.map(_.min).mkString("-"), b.map(_.min).mkString("-")).toString()
+}
+
+class TestCPConstraintConversion extends TestSuite {
+  test("CP Constraint Conversion - std types") {
+    val ist = ConvertCPConstraint.getConstraint[StdTypeTest](1, 1L, "1", 1.0d, true, 1.0)
+    assert(ist.print == "(1,1,1,1.0,true,1.0)")
+  }
+
+  test("CP Constraint Conversion - std types with collections") {
+    val ist = ConvertCPConstraint.getConstraint[StdCollectionsTest](Array[Int](1,2), Set(1L, 2L), Seq(true, false))
+    assert(ist.print == "(1-2,1-2,true-false)")
+  }
+
+  implicit val modelDecl = new ModelDeclaration()
+  val intvars = Array.fill(2)(IntVar(2, 4))
+  val boolvars = Seq.fill(2)(BoolVar())
+  val cpModel = CPInstantiate(modelDecl.getCurrentModel.asInstanceOf[UninstantiatedModel])
+
+  test("CP Constraint Conversion - intvar and boolvar") {
+    modelDecl.apply(cpModel) {
+      val ist = ConvertCPConstraint.getConstraint[IntVarTest](intvars.head, boolvars.head)
+      assert(ist.print == "(2,0)")
+    }
+  }
+
+  test("CP Constraint Conversion - intvar and boolvar in collections") {
+    modelDecl.apply(cpModel) {
+      val ist = ConvertCPConstraint.getConstraint[ArrayTest](intvars, boolvars)
+      assert(ist.print == "(2-2,0-0)")
+    }
+  }
+
+}
