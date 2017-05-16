dec1928c317a52e5290b784d502b74fd54c65239
Germeau Fabian <devnull@localhost>
2017-05-16 14:25:07 +0200

    Remove no more used files

--- oscar-cbls/src/main/scala/oscar/cbls/business/routing/legacy/model/PDP.scala
+++ oscar-cbls/src/main/scala/oscar/cbls/business/routing/legacy/model/PDP.scala
@@ -1,4 +1,4 @@
-package oscar.cbls.business.routing.model
+package oscar.cbls.business.routing.legacy.model
 
 /*******************************************************************************
   * OscaR is free software: you can redistribute it and/or modify
