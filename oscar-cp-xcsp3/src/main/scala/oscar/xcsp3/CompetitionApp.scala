e35d1f680262ca423162e028e598492c7cacb398
Charles Thomas <charles.thomas@uclouvain.be>
2017-05-26 11:03:12 +0200

    Output functions added

--- oscar-cp-xcsp3/src/main/scala/oscar/xcsp3/CompetitionApp.scala
+++ oscar-cp-xcsp3/src/main/scala/oscar/xcsp3/CompetitionApp.scala
@@ -15,14 +15,28 @@ class CompetitionConf(arguments: Seq[String]) extends ScallopConf(arguments){
 
 object CompetitionApp extends App{
   val conf = new CompetitionConf(args)
-  println("args: ")
-  println("Bench name: " + conf.benchname())
-  println("Random seed: " + (if(conf.randomseed.isDefined) conf.randomseed() else "None"))
-  println("Time limit: " + (if(conf.timelimit.isDefined) conf.timelimit() else "None"))
-  println("Mem limit: " + (if(conf.memlimit.isDefined) conf.memlimit() else "None"))
-  println("Nb cores: " + (if(conf.nbcore.isDefined) conf.nbcore() else "None"))
 
-  //TODO: parse bench file
+  //Parsing instance:
+  val parser = XCSP3Parser(conf.benchname())
 
-  //TODO: Launch search!
+  //TODO: Launch search
+
+  def printObjective(obj: Int): Unit = println("o " + obj)
+
+  def printSolution(sol: String, optimum:Boolean = false): Unit = {
+    if(optimum) println("s OPTIMUM FOUND")
+    else println("s SATISFIABLE")
+
+    println("v " + sol.split("\\r?\\n").mkString("\nv "))
+  }
+
+  /**
+    * Allowed status:
+    * UNSATISFIABLE: the instance is unsatisfiable
+    * UNSUPPORTED: unsupported constraint
+    * UNKNOWN: other problem
+    */
+  def printStatus(status: String): Unit = println("s " + status)
+
+  def printComment(com: String): Unit = println("c " + com.split("\\r?\\n").mkString("\nc "))
 }
