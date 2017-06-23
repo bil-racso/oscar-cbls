3c4937f0ef69b88c881d1f300de8d79130f79aef
Renaud De Landtsheer <devnull@localhost>
2017-06-23 12:01:10 +0200

    drafting new invar

--- oscar-cbls/src/main/scala/oscar/cbls/lib/invariant/set/IncludedSubsets.scala
+++ oscar-cbls/src/main/scala/oscar/cbls/lib/invariant/set/IncludedSubsets.scala
@@ -1,13 +1,14 @@
 package oscar.cbls.lib.invariant.set
 
 import oscar.cbls.algo.quick.QList
-import oscar.cbls.core.computation.{ChangingSetValue, SetNotificationTarget, IntInvariant, SetValue}
+import oscar.cbls.core.computation._
 import oscar.cbls.core.propagation.Checker
 
 import scala.collection.immutable.SortedSet
 
 /**
- * @param s
+ * sum(on s:subsetToMonitorAndMaxValues if #(s._1 inter s) > s._2 of s._3 )
+ * @param s a setValue
  * @param subsetToMonitorAndMaxValues iterable of (subset, max occurrence in the subset, weight in case of violation)
  */
 case class IncludedSubsets(s: SetValue, subsetToMonitorAndMaxValues:Iterable[(Iterable[Int],Int,Int)])
@@ -66,8 +67,70 @@ case class IncludedSubsets(s: SetValue, subsetToMonitorAndMaxValues:Iterable[(It
   override def checkInternals(c: Checker) {
     val violation = subsetToMonitorAndMaxValues.map({case (values,maxValue,weight) => if(values.count(v => s.value.contains(v)) > maxValue) weight else 0}).sum
     c.check(this.value == violation,Some("included subset Error value=" + this.value  + " should be:" + violation))
+  }
+}
 
 
+/**
+ * @param s
+ * @param subsetToMonitorAndMaxOcc iterable of (subset, max occurrence in the subset)
+ */
+case class PossibleExtensions(s: SetValue, allVal:SortedSet[Int], subsetToMonitorAndMaxOcc:Iterable[(Iterable[Int],Int)])
+  extends SetInvariant(initialDomain = Domain(allVal))
+  with SetNotificationTarget{
+
+  registerStaticAndDynamicDependenciesNoID(s)
+  finishInitialization()
+
+  require(s.min == 0)
+
+  val subsetAndMax = subsetToMonitorAndMaxOcc.toArray
+  val n = subsetAndMax.length
+
+  val valueToClauseIDs:Array[QList[Int]] = Array.fill(s.max+1)(null)
+
+  for (clauseID <- subsetAndMax.indices) {
+    val (values,maxNumber,weight) = subsetAndMax(clauseID)
+    for(value <- values){
+      valueToClauseIDs(value) = QList(clauseID,valueToClauseIDs(value))
+    }
+  }
+
+  this := allVal
+  val subsetToNbPresent:Array[Int] = Array.fill(n)(0)
+  for(value <- s.value){
+    notifyInsert(value)
+  }
+
+  override def notifySetChanges(v: ChangingSetValue, d: Int, addedValues: Iterable[Int],
+                                removedValues: Iterable[Int], oldValue: SortedSet[Int], newValue: SortedSet[Int]) : Unit = {
+    for (added <- addedValues) notifyInsert(added)
+    for (deleted <- removedValues) notifyDelete(deleted)
+  }
+
+  @inline
+  private def notifyInsert(value: Int) {
+    for(subset <- QList.toIterable(valueToClauseIDs(value))){
+      subsetToNbPresent(subset) = subsetToNbPresent(subset) + 1
+      if(subsetToNbPresent(subset) == subsetAndMaxAndWeightArray(subset)._2 + 1) {
+        this :+= subsetAndMaxAndWeightArray(subset)._3
+      }
+    }
+  }
+
+  @inline
+  private def notifyDelete(value: Int) {
+    for(subset <- QList.toIterable(valueToClauseIDs(value))){
+      subsetToNbPresent(subset) = subsetToNbPresent(subset) - 1
+      if(subsetToNbPresent(subset) == subsetAndMaxAndWeightArray(subset)._2){
+        this :-= subsetAndMaxAndWeightArray(subset)._3
+      }
+    }
+  }
+
+  override def checkInternals(c: Checker) {
+    val violation = subsetToMonitorAndMaxValues.map({case (values,maxValue,weight) => if(values.count(v => s.value.contains(v)) > maxValue) weight else 0}).sum
+    c.check(this.value == violation,Some("included subset Error value=" + this.value  + " should be:" + violation))
   }
 }
 
