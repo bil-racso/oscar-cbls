64b8fe0ee3b72d7a77071bf6e08fab25fe64b3ce
Germeau Fabian <devnull@localhost>
2017-10-02 12:01:06 +0200

    add contentFlow and vehicleSize genrators + comment all the RoutingMatrixGenerator methods

--- oscar-cbls/src/main/examples/oscar/examples/cbls/routing/RoutingMatrixGenerator.scala
+++ oscar-cbls/src/main/examples/oscar/examples/cbls/routing/RoutingMatrixGenerator.scala
@@ -23,6 +23,13 @@ import scala.util.Random
 object RoutingMatrixGenerator {
   val random = new Random(0)
 
+  /**
+    * This method generate a random distance matrix based on numbers of node and map side.
+    * It also generate an array of node positions. (Usefull when you want to display it on a map)
+    * @param N The number of nodes (considering depots)
+    * @param side The side of the map
+    * @return The distance matrix (Array[Array[Int] ] and the position of each node (Array[(Int,Int)])
+    */
   def apply(N: Int, side: Int = 1000): (Array[Array[Int]],Array[(Int,Int)]) = {
 
     //we generate te cost distance matrix
@@ -38,6 +45,15 @@ object RoutingMatrixGenerator {
         n2 => distance(pointPosition(n1), pointPosition(n2)))),pointPosition)
   }
 
+  /**
+    * This method generate random restrictions for the problem.
+    * A restriction is a tuple of a vehicle and a node.
+    * This means that if the specified node is routed, it must be on the route of the specified vehicle
+    * @param n The number of nodes (considering depots)
+    * @param v The number of vehciles/depots
+    * @param nbRestrictions The number of restrictions we want to generate
+    * @return An iterable of tuple (node, vehicle)
+    */
   def generateRestrictions(n:Int,v:Int,nbRestrictions:Int):Iterable[(Int,Int)] = {
     var toReturn = List.empty[(Int,Int)]
 
@@ -51,6 +67,13 @@ object RoutingMatrixGenerator {
     toReturn
   }
 
+  /**
+    * Generate random precedences of size 2
+    * @param n The number of nodes (considering depots)
+    * @param v The number of vehicles/depots
+    * @param nbPRecedences The number wanted precedences
+    * @return A list of tuple (precedences)
+    */
   def generatePrecedence(n:Int,v:Int,nbPRecedences:Int):List[(Int,Int)] = {
     val allNodes = v until n
 
@@ -66,7 +89,13 @@ object RoutingMatrixGenerator {
     toReturn
   }
 
-  def addLonelyNodesToPrecedences(n: Int, precedences: List[List[Int]]): List[List[Int]]={
+  /**
+    * This method creates a List(node) for each node that doesn't belong to any precedence.
+    * @param n The number of node (considering depot)
+    * @param precedences The precedences of the problem
+    * @return An updated precedences list (with lonely nodes)
+    */
+  private def addLonelyNodesToPrecedences(n: Int, precedences: List[List[Int]]): List[List[Int]]={
     val lonelyNodes = Range(0,n).toList.diff(precedences.flatten)
     var precedencesWithLonelyNodes = precedences
     for(lonelyNode <- lonelyNodes)
@@ -74,6 +103,24 @@ object RoutingMatrixGenerator {
     precedencesWithLonelyNodes
   }
 
+  /**
+    * This method generate a time window for each node such that the resolver can find a solution for the problem.
+    *
+    * It works like this :
+    *   We add each precedence to a randomly selected vehicle.
+    *     We randomly generate the earlyline, taskDuration and deadline for each node of the precedence
+    *
+    * This way there is always at least one solution for the problem.
+    *
+    * @param n The number of node of the problem (considering depot)
+    * @param v The number of vehicle/depot
+    * @param timeMatrix The travel time matrix
+    * @param precedences The list of precedences
+    * @param maxIdlingTimeInSec The maximum idling time (the time between two precedences)
+    * @param maxExtraTravelTimeInSec The maximum extra travel time (time added to earlylines in order to have a more permissive time window)
+    * @param maxTaskDurationInSec The maximum duration of a task
+    * @return The time window for each node
+    */
   def generateFeasibleTimeWindows(n:Int, v:Int,
                                   timeMatrix: TTFMatrix,
                                   precedences: List[List[Int]] = List.empty,
@@ -129,6 +176,24 @@ object RoutingMatrixGenerator {
     (earlyLines,deadLines,taskDurations,maxWaitingDuration)
   }
 
+  /**
+    * Based on precedences and earlylines, this methods generate a map of maxTravelDuration.
+    * These values, when added to the constraints system ensure that the travel duration between A and B
+    * doesn't exceeds a specified value.
+    *
+    * This generator set the maxTravelDuration's values like this :
+    *
+    * (travelDuration between A and B)*1.5
+    *
+    * e.g.: If the travel duration between A and B is 600, then the maxTravelDuration will be 900.
+    * It means that the resolver can add a node (C) between A and B only if :
+    *   leaveTime(A) + travel duration to C + taskDuration at C + travel duration to B <= 900
+    *
+    * @param precedences The list of precedences
+    * @param earlylines The array of earlylines (We can't start the task at this node before the earlyline value)
+    * @param travelDurationMatrix The travel time matrix
+    * @return A Map[(from,to) -> maxTravelDuration]
+    */
   def generateMaxTravelDurations(precedences: List[List[Int]],
                                  earlylines: Array[Int],
                                  travelDurationMatrix: TTFMatrix): Map[(Int,Int),Int] ={
@@ -143,12 +208,72 @@ object RoutingMatrixGenerator {
     precedences.flatMap(p => maxTravelDurationOfPrecedence(p.head,p.tail,List.empty)).toMap
   }
 
+  /**
+    * Base on the distance matrix, this method generate a linear travel time matrix.
+    *
+    * e.g. :  859 distance units to go from A to B => 859 time units to go from A to B
+    * @param n The nb of node in the problem (considering depots)
+    * @param distanceMatrix The distance matrix (of size n*n)
+    * @return A TTFMatrix object that represent the travel time matrix.
+    */
   def generateLinearTravelTimeFunction(n:Int,distanceMatrix: Array[Array[Int]]): TTFMatrix = {
     val ttf = new TTFMatrix(n, new TTFConst(500))
     for (i <- 0 until n)
       for (j <- 0 until n)
         ttf.setTTF(i, j, new TTFConst(distanceMatrix(i)(j)))
     ttf
+  }
+
+  /**
+    * This method return a random array of content flow.
+    * Meaning when a vehicle arrives at a particular node, we add the value contentFlow(node) to the vehicle content.
+    * This value is allways positive for heads of precedence (List[Int]) and always negative for last of precedence.
+    * In between it's positive of negative. (random)
+    * But the summed value of each precedence (Lisŧ[Int]) is equal to 0.
+    *
+    * By default, the content flow at depot is equal to zero.
+    *
+    * e.g.: +4, +2, -3, +5, -4, -4   or   +2, 0, 0, -2
+    *
+    * @param n The number of node of the problem
+    * @param precedences The list of precedences (List[List[Int] ])
+    * @param maxVehicleSize The max size of all vehicles in the problem (All the vehicles don't have to have the same size)
+    * @return An array of Int that represents the vehicle content evolution when arriving at a given node.
+    */
+  def generateContentFlow(n: Int, precedences: List[List[Int]], maxVehicleSize: Int): Array[Int] ={
+    def randomContent(currentMaxContent: Int) = random.nextInt(currentMaxContent)
+    def isPickupStep: Boolean = random.nextBoolean()
+
+    val contentFlow = Array.fill(n)(0)
+    for(precedence <- precedences){
+      var currentPrecedenceContent = 0
+      for (node <- precedence){
+        if(node == precedence.head) {
+          contentFlow(node) = randomContent(maxVehicleSize)
+          currentPrecedenceContent += contentFlow(node)
+        } else if(node == precedence.last) {
+          contentFlow(node) = -currentPrecedenceContent
+        } else {
+          contentFlow(node) =
+            if(isPickupStep) randomContent(maxVehicleSize-currentPrecedenceContent)
+            else -randomContent(currentPrecedenceContent)
+          currentPrecedenceContent += contentFlow(node)
+        }
+      }
+    }
+
+    contentFlow
+  }
+
+  /**
+    * This method generate a random array of vehicle size
+    *
+    * @param v the number of vehicle
+    * @param maxVehicleSize the maximum vehicle size
+    * @return an array of vehicle size
+    */
+  def generateVehiclesSize(v: Int, maxVehicleSize: Int): Array[Int] ={
+    Array.fill(v)(random.nextInt(maxVehicleSize))
   }
 }
 
