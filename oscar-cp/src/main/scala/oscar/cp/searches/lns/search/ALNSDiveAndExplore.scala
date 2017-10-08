8820e6595b299ece6cd6db8da0f5a0b10f1e67f2
Charles Thomas <charles.thomas@uclouvain.be>
2017-10-08 23:20:15 +0200

    Added meta operators management and dynamical efficiency threshold in DiveAndExplore ALNS

--- oscar-cp/src/main/scala/oscar/cp/searches/lns/search/ALNSDiveAndExplore.scala
+++ oscar-cp/src/main/scala/oscar/cp/searches/lns/search/ALNSDiveAndExplore.scala
@@ -1,7 +1,8 @@
 package oscar.cp.searches.lns.search
 
+import oscar.algo.Inconsistency
 import oscar.cp.{CPIntVar, CPSolver}
-import oscar.cp.searches.lns.operators.{ALNSElement, ALNSOperator}
+import oscar.cp.searches.lns.operators.{ALNSElement, ALNSOperator, ALNSReifiedOperator}
 import oscar.cp.searches.lns.selection.{Metrics, PriorityStore, RouletteWheel}
 
 import scala.util.Random
@@ -10,14 +11,17 @@ import scala.util.Random
   * TODO
   */
 class ALNSDiveAndExplore(solver: CPSolver, vars: Array[CPIntVar], config: ALNSConfig) extends ALNSSearchImpl(solver, vars, config){
-  lazy val efficiencyEvalTime: Long = 2000000000L //(config.timeout/nOpCombinations.toDouble).ceil.toLong
-  lazy val minEfficiencyThreshold: Double = 0.5 //Math.abs(bestSol.get.objective - previousBest.get.objective) / (efficiencyEvalTime.toDouble * 10)
+//  lazy val efficiencyEvalTime: Long = iterTimeout * 2 //(config.timeout/nOpCombinations.toDouble).ceil.toLong
+//  lazy val minEfficiencyThreshold: Double = 0.5 //Math.abs(bestSol.get.objective - previousBest.get.objective) / (efficiencyEvalTime.toDouble * 10)
+  lazy val tolerance = 0.5
+  lazy val efficiencyEvalIters = 5
 
   override def alnsLoop(): Unit = {
     if (!solver.silent){
       println("\nStarting adaptive LNS...")
       println("n operators: " + nOpCombinations)
-      println("Efficiency Threshold: " + minEfficiencyThreshold)
+//      println("Efficiency Threshold: " + minEfficiencyThreshold)
+      println("tolerance: " + tolerance)
     }
     if(!solver.silent) println("\nStarting dive...")
     dive()
@@ -28,14 +32,20 @@ class ALNSDiveAndExplore(solver: CPSolver, vars: Array[CPIntVar], config: ALNSCo
   def timeLearning(): Unit = {
   }
 
-  private def dive(): Unit = {
+  protected def dive(): Unit = {
     //Running each op combination to evaluate TTI:
+    learning = true
+    iterTimeout = config.timeout
     for {
       relax <- Random.shuffle(getReifiedOperators(relaxOps))
       search <- Random.shuffle(getReifiedOperators(searchOps))
     } {
       lnsIter(relax, search)
     }
+    learning = false
+
+    manageIterTimeout()
+    println("learning done, iterTimeout: " + iterTimeout)
 
     //Diving:
     val relaxPriority = new PriorityStore[ALNSOperator](relaxOps, relaxOps.map(Metrics.timeToImprovement), 1.0, true, Metrics.timeToImprovement)
@@ -58,7 +68,7 @@ class ALNSDiveAndExplore(solver: CPSolver, vars: Array[CPIntVar], config: ALNSCo
     }
   }
 
-  private def explore(): Unit = {
+  protected def explore(): Unit = {
     val relaxRWheel = new RouletteWheel[ALNSOperator](relaxOps.filter(_.isActive), relaxOps.filter(_.isActive).map(multiArmedBandit), 1.0, false, multiArmedBandit)
     val searchRWheel = new RouletteWheel[ALNSOperator](searchOps.filter(_.isActive), searchOps.filter(_.isActive).map(multiArmedBandit), 1.0, false, multiArmedBandit)
     while(System.nanoTime() < endTime && relaxRWheel.nonActiveEmpty && searchRWheel.nonActiveEmpty && !optimumFound){
@@ -72,18 +82,115 @@ class ALNSDiveAndExplore(solver: CPSolver, vars: Array[CPIntVar], config: ALNSCo
     }
   }
 
-  private def multiArmedBandit(elem: ALNSElement): Double = {
-    elem.efficiency(efficiencyEvalTime) + Math.sqrt((2 * Math.log(iter))/elem.execs)
+  protected def multiArmedBandit(elem: ALNSElement): Double = {
+    elem.efficiency(iterTimeout) + Math.sqrt((2 * Math.log(iter))/elem.execs)
   }
 
-  private def checkEfficiency(op: ALNSOperator): Unit = {
-    if(op.name != "dummy" && op.time >= efficiencyEvalTime) {
-      val efficiency = op.efficiency(efficiencyEvalTime)
+  protected def checkEfficiency(op: ALNSOperator): Unit = {
+    if(op.name != "dummy" && op.time >= iterTimeout) {
+      val efficiency = op.efficiency(iterTimeout * efficiencyEvalIters)
       if(!solver.silent) println("Operator " + op.name + " efficiency is " + efficiency)
-      if (efficiency < minEfficiencyThreshold) {
+      if (efficiency < searchEfficiency(iterTimeout * efficiencyEvalIters) * tolerance) {
         op.setActive(false)
         if (!solver.silent) println("Operator " + op.name + " deactivated due to low efficiency!")
+        manageIterTimeout()
       }
     }
+  }
+
+  protected def searchEfficiency(t: Long): Double = {
+    if(solsFound.isEmpty) 0.0
+    else {
+      val currentTime = System.nanoTime()
+      val currentObj = solsFound.last.objective
+      var tStart = currentTime
+      var objStart = currentObj
+      val sols = solsFound.reverseIterator
+      while (sols.hasNext && currentTime - tStart < t) {
+        val nextSol = sols.next()
+        tStart = nextSol.time
+        objStart = nextSol.objective
+      }
+      objStart.toDouble / tStart.toDouble
+    }
+  }
+
+  protected def manageIterTimeout(): Unit = {
+    var maxTime = 0L
+    searchOps.filter(op =>{ op.isActive && op.execs > 0}).foreach(op =>{
+      val avgTime = Metrics.avgTime(op)
+      if(avgTime > maxTime) maxTime = avgTime.ceil.toLong
+    })
+    iterTimeout = maxTime
+  }
+
+  override protected def lnsIter(relax: ALNSOperator, search: ALNSOperator): Unit = {
+    if(!learning) endIter = Math.min(System.nanoTime() + iterTimeout, endTime)
+
+    if(!solver.silent){
+      println("\nStarting new search with: " + relax.name + " and " + search.name)
+      println("Operator timeout: " + (endIter - System.nanoTime())/1000000000.0 + "s")
+    }
+
+    //New search using selected strategies:
+    val (relaxFunction, _, _) = relax.getFunction
+    val (searchFunction, searchFailures, searchDiscrepancy) = search.getFunction
+    if(searchFailures.isDefined) nFailures = searchFailures.get
+
+    do {
+      var relaxDone = true
+      val oldObjective = currentSol.get.objective
+      val iterStart = System.nanoTime()
+
+      val stats = solver.startSubjectTo(stopCondition, searchDiscrepancy.getOrElse(Int.MaxValue), null) {
+        try {
+          relaxFunction(currentSol.get)
+          searchFunction(currentSol.get)
+        }
+        catch {
+          case _: Inconsistency => relaxDone = false
+        }
+      }
+
+      val iterEnd = System.nanoTime()
+      val newObjective = currentSol.get.objective
+
+      if(searchFailures.isDefined) nFailures = 0 //Resetting failures number to 0
+
+      val improvement = math.abs(currentSol.get.objective - oldObjective)
+      val time = iterEnd - iterStart
+
+      if(improvement > 0) stagnation = 0
+      else stagnation += 1
+
+      if (!solver.silent){
+        if(!relaxDone) println("Search space empty, search not applied, improvement: " + improvement)
+        else if(stats.completed) println("Search space completely explored, improvement: " + improvement)
+        else println("Search done, Improvement: " + improvement)
+      }
+
+      //Updating probability distributions:
+      relax.update(iterStart, iterEnd, oldObjective, newObjective, stats, fail = !relaxDone && !learning, iter)
+      if(!relax.isInstanceOf[ALNSReifiedOperator]){
+        if (relax.isActive) relaxStore.adapt(relax)
+        else {
+          if (!solver.silent) println("Operator " + relax.name + " deactivated")
+          if (!learning) relaxStore.deactivate(relax)
+        }
+      }
+
+      if(relaxDone || relax.name == "dummy"){
+        search.update(iterStart, iterEnd, oldObjective, newObjective, stats, fail = false, iter)
+        if(!search.isInstanceOf[ALNSReifiedOperator]){
+          if (search.isActive) searchStore.adapt(search)
+          else {
+            if (!solver.silent) println("Operator " + search.name + " deactivated")
+            if (!learning) searchStore.deactivate(search)
+          }
+        }
+      }
+    }while(System.nanoTime() < endIter && !learning)
+
+    iter += 1
   }
 }
