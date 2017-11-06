216cce67150d1daf6b135b317cf47ff01f69dea7
Dimitri Durieux <dimitri.durieux@cetic.be>
2017-11-06 14:19:54 +0000

    Attempt to fix versionning issue in artifactory

--- project/OscarBuild.scala
+++ project/OscarBuild.scala
@@ -96,9 +96,9 @@ object OscarBuild extends Build {
       publishTo := {
         val artifactory = "http://maven.oscar.ext.cetic.be:8081/artifactory/"
         if (isSnapshot.value)
-          Some("Artifactory Realm" at artifactory + "sbt-dev;build.timestamp=" + new java.util.Date().getTime)
+          Some("Artifactory Realm" at artifactory + "libs-snapshot-local;build.timestamp=" + new java.util.Date().getTime)
         else
-          Some("Artifactory Realm" at artifactory + "sbt-release")
+          Some("Artifactory Realm" at artifactory + "libs-release-local")
       },
       packageOptions += Package.ManifestAttributes(
         ("REVISION_ID", System.getProperty("REVISION_ID")),
