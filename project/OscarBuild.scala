b5cafd2bdf40d603eb6ec17be1719de67ae61f99
Dimitri Durieux <dimitri.durieux@cetic.be>
2017-11-09 08:49:11 +0000

    Attempt to switch to ivy to deal with sbt repository problems

--- project/OscarBuild.scala
+++ project/OscarBuild.scala
@@ -96,9 +96,9 @@ object OscarBuild extends Build {
       publishTo := {
         val artifactory = "http://maven.oscar.ext.cetic.be:8081/artifactory/"
         if (isSnapshot.value)
-          Some("Artifactory Realm" at artifactory + "sbt-dev;build.timestamp=" + new java.util.Date().getTime)
+          Some("Artifactory Realm" at artifactory + "ivy-dev;build.timestamp=" + new java.util.Date().getTime)
         else
-          Some("Artifactory Realm" at artifactory + "sbt-release")
+          Some("Artifactory Realm" at artifactory + "iby-release")
       },
       packageOptions += Package.ManifestAttributes(
         ("REVISION_ID", System.getProperty("REVISION_ID")),
