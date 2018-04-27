67a0864ebc5c4f8801203ce9d6b8ef02e15bd1e2
Charles Thomas <charles.thomas@uclouvain.be>
2018-04-27 17:50:39 +0200

    fixing COSSolver for test scripts

---
+++ oscar-cp-xcsp3/src/main/scala/oscar/xcsp3/competition/solvers/ConfOrderSolver.scala
@@ -0,0 +1,5 @@
+package oscar.xcsp3.competition.solvers
+
+import oscar.xcsp3.competition.solvers.resources.COSSolver
+
+object ConfOrderSolver extends COSSolver
--- oscar-cp-xcsp3/src/main/scala/oscar/xcsp3/competition/solvers/resources/COSSolver.scala
+++ oscar-cp-xcsp3/src/main/scala/oscar/xcsp3/competition/solvers/resources/COSSolver.scala
@@ -1,13 +1,13 @@
-package oscar.xcsp3.competition.solvers
+package oscar.xcsp3.competition.solvers.resources
 
 import oscar.algo.Inconsistency
 import oscar.algo.search.{DFSearch, SearchStatistics}
-import oscar.cp.{CPSolver, _}
 import oscar.cp.core.variables.CPIntVar
 import oscar.cp.searches.lns.CPIntSol
-import oscar.modeling.models.{ModelDeclaration, UninstantiatedModel}
+import oscar.cp.{CPSolver, _}
 import oscar.modeling.models.cp.CPModel
 import oscar.modeling.models.operators.CPInstantiate
+import oscar.modeling.models.{ModelDeclaration, UninstantiatedModel}
 import oscar.xcsp3.XCSP3Parser2
 import oscar.xcsp3.competition.{CompetitionApp, CompetitionConf}
 
@@ -97,60 +97,3 @@ abstract class COSSolver extends CompetitionApp with App{
   }
 }
 
-object ConfOrderSolver extends COSSolver
-
-object ConfOrderSolverWithRestarts extends COSSolver {
-
-  override def startSearch(solver: CPSolver, stopCondition: DFSearch => Boolean, vars: Array[CPIntVar], maximizeObjective: Option[Boolean]): SearchStatistics = {
-
-    val initialTimeLimit : Long = 1000000000L
-    var restartTimeLimit : Long = initialTimeLimit
-    var globalStop = false
-    var lastSearchStats : SearchStatistics = null
-    var initialTime = System.nanoTime()
-    val r = 1.3
-    var completed = false
-
-    val stopRestartCondition : DFSearch => Boolean = search => {
-      val initialStop = stopCondition(search)
-      if(initialStop)
-       globalStop = true
-
-      val stopRestart = restartTimeLimit < System.nanoTime() - initialTime
-      initialStop || stopRestart || completed
-    }
-
-    val rand = new scala.util.Random(42)
-
-    solver.search(
-      conflictOrderingSearch(
-        vars,
-        i => vars(i).size,
-//        learnValueHeuristic(vars, if(maximizeObjective.isDefined) if(maximizeObjective.get) vars(_).max else vars(_).min else vars(_).max)
-        learnValueHeuristic(vars, if(maximizeObjective.isDefined) if(maximizeObjective.get) vars(_).randomValue(rand) else vars(_).randomValue(rand) else vars(_).randomValue(rand))
-      )
-    )
-
-    //find first solution
-    lastSearchStats = solver.start(timeLimit = 10, nSols = 1)
-    if(lastSearchStats.completed) {
-      completed = true
-    }
-
-    //restarts
-    while(!globalStop) {
-//      println(s"restartTimeLimit : $restartTimeLimit")
-      initialTime = System.nanoTime()
-      lastSearchStats = solver.startSubjectTo(stopRestartCondition, Int.MaxValue, null){}
-      if(lastSearchStats.completed) {
-        completed = true
-      }
-      if(lastSearchStats.nSols > 0) {
-        restartTimeLimit = initialTimeLimit
-      }
-      restartTimeLimit = math.ceil(restartTimeLimit * r).toLong
-    }
-
-    lastSearchStats
-  }
-}
