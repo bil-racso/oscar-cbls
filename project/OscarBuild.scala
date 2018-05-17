fcf00f14d243a1faa7faff5bfba93781b2ecd7e5
helene_verhaeghe <devnull@localhost>
2018-05-17 11:21:32 +0200

    Real update to sbt 1.1.4 (change sbtVersion in OscarBuild don't change sbt version, has to be changed in build.property)
    Changes of structure : projects are defined in build.sbt at root of project,
    common settings and Dependencies stays in OscarBuild.scala
    Every thing compile and test. There is still a warning during build
    I can't get rid of. Had to add depedency to scala-xml explicit for OscalVisual.
    Main project has to be called root.
    
    Some changes for the commands : changes of naming for sub project
    (ex: instead of oscar-cp/compile, use oscarCp/compile).

--- project/OscarBuild.scala
+++ project/OscarBuild.scala
@@ -1,53 +1,49 @@
 package oscar
 
-import sbt._
 import sbt.Keys._
-import de.johoop.jacoco4sbt.JacocoPlugin._
-import xerial.sbt.Pack._
-import sbtunidoc.Plugin._
+import sbt._
 
 
-object OscarBuild extends Build {
 
-  lazy val PerfTest = config("perf") extend(Test)
+object OscarBuild {
 
-  object BuildSettings {
-    val buildOrganization = "oscar"
-    val buildVersion = "4.0.0-SNAPSHOT"
-    val buildScalaVersion = "2.12.4"
-    val buildSbtVersion= "1.1.4"
+  lazy val PerfTest = config("perf") extend (Test)
 
+  lazy val buildSettings = Seq(
+    organization := "oscar",
+    version := "4.1.0-SNAPSHOT",
+    scalaVersion := "2.12.4",
+    sbtVersion := "1.1.4"
+  )
 
-    lazy val commonSettings = Defaults.defaultSettings ++  jacoco.settings ++ Seq(
-      organization := buildOrganization,
-      version := buildVersion,
-      scalacOptions in Compile ++= Seq("-encoding", "UTF-8", "-deprecation", "-feature",
-                                       "-unchecked", "-Xdisable-assertions", "-language:implicitConversions",
-                                       "-language:postfixOps"),
-      scalacOptions in Test := Seq("-optimise"),
-      testOptions in Test <+= (target in Test) map {
-        t => Tests.Argument(TestFrameworks.ScalaTest, "junitxml(directory=\"%s\")" format (t / "test-reports") ) },
-      parallelExecution in Test := false,
-      fork in Test := true,
-      javaOptions in Test += "-Djava.library.path=../lib:../lib/",
-      javacOptions ++= Seq("-encoding", "UTF-8"),
-      scalaVersion := buildScalaVersion,
-      unmanagedSourceDirectories in Test += baseDirectory.value / "src" / "main" / "examples",
-      publishTo := {
-        val artifactoryName = "Artifactory Realm"
-        val artifactoryUrl = "http://130.104.230.89/artifactory/"
-        if (isSnapshot.value)
-          Some(artifactoryName at artifactoryUrl + "libs-snapshot-local;build.timestamp=" + new java.util.Date().getTime)
-        else
-          Some(artifactoryName at artifactoryUrl + "libs-release-local")
-      },
-      credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
-      testOptions in PerfTest <+= (target in PerfTest) map {
-        t => Tests.Argument(TestFrameworks.ScalaTest, "junitxml(directory=\"%s\")" format (t / "test-reports") ) },
-      fork in PerfTest := true,
-      parallelExecution in PerfTest := false
-    )
-  }
+  lazy val commonSettings = buildSettings ++ Defaults.coreDefaultSettings ++ Seq(
+    scalacOptions in Compile ++= Seq("-encoding", "UTF-8", "-deprecation", "-feature",
+      "-unchecked", "-Xdisable-assertions", "-language:implicitConversions",
+      "-language:postfixOps"),
+    scalacOptions in Test := Seq("-optimise"),
+    testOptions in Test += ((target in Test) map {
+      t => Tests.Argument(TestFrameworks.ScalaTest, "junitxml(directory=\"%s\")" format (t / "test-reports"))
+    }).value,
+    parallelExecution in Test := false,
+    fork in Test := true,
+    javaOptions in Test += "-Djava.library.path=../lib:../lib/",
+    javacOptions ++= Seq("-encoding", "UTF-8"),
+    unmanagedSourceDirectories in Test += baseDirectory.value / "src" / "main" / "examples",
+    publishTo := {
+      val artifactoryName = "Artifactory Realm"
+      val artifactoryUrl = "http://130.104.230.89/artifactory/"
+      if (isSnapshot.value)
+        Some(artifactoryName at artifactoryUrl + "libs-snapshot-local;build.timestamp=" + new java.util.Date().getTime)
+      else
+        Some(artifactoryName at artifactoryUrl + "libs-release-local")
+    },
+    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
+    testOptions in PerfTest += ((target in PerfTest) map {
+      t => Tests.Argument(TestFrameworks.ScalaTest, "junitxml(directory=\"%s\")" format (t / "test-reports"))
+    }).value,
+    fork in PerfTest := true,
+    parallelExecution in PerfTest := false
+  )
 
   object Resolvers {
     val xypron = "Xypron Release" at "http://rsync.xypron.de/repository/"
@@ -63,198 +59,33 @@ object OscarBuild extends Build {
     val jfreechart = "org.jfree" % "jfreechart" % "latest.milestone"
     val jsci = "net.sf.jsci" % "jsci" % "latest.milestone"
     val scalaParserCombinators = "org.scala-lang.modules" %% "scala-parser-combinators" % "latest.milestone"
-    val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "latest.milestone"
+    val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "1.1.0"
     val scalaSwing = "org.scala-lang.modules" %% "scala-swing" % "latest.milestone"
-    val swingx = "org.swinglabs" % "swingx" % "1.0"
+    val swingx = "org.swinglabs" % "swingx" % "latest.milestone"//"1.0"
     val swingxWs = "org.swinglabs" % "swingx-ws" % "1.0"
     val xmlApisExt = "xml-apis" % "xml-apis-ext" % "latest.milestone"
-    val xcsp3 = "xcsp3"  % "xcsp3" % "1.0.0-SNAPSHOT"
+    //    val xcsp3 = "xcsp3"  % "xcsp3" % "1.0.0-SNAPSHOT"
+    val xcsp3 = "org.xcsp" % "xcsp3-tools" % "1.0.0"
     val graphStreamCore = "org.graphstream" % "gs-core" % "1.3"
     val graphStreamAlgo = "org.graphstream" % "gs-algo" % "1.3"
     val graphStreamUI = "org.graphstream" % "gs-ui" % "1.3"
     val scallop = "org.rogach" % "scallop_2.11" % "1.0.0"
 
     // Akka
-    val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.5.6"
-    val akkaRemote = "com.typesafe.akka" %% "akka-remote" % "2.5.6"
+    val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.5.12"
+    val akkaRemote = "com.typesafe.akka" %% "akka-remote" % "2.5.12"
 
     // Test libraries
     val junit = "junit" % "junit" % "latest.milestone" % Test
-    val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.13.+" % Test
-    val scalaTest = "org.scalatest" %% "scalatest" % "3.0.4" % Test
+    val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.14.0" % Test
+    val scalaTest = "org.scalatest" %% "scalatest" % "3.2.0-SNAP10" % Test
 
     val junit2 = "junit" % "junit" % "latest.milestone" % PerfTest
-    val scalaCheck2 = "org.scalacheck" %% "scalacheck" % "1.13.+" % PerfTest
-    val scalaTest2 = "org.scalatest" %% "scalatest" % "3.0.4" % PerfTest
+    val scalaCheck2 = "org.scalacheck" %% "scalacheck" % "1.14.0" % PerfTest
+    val scalaTest2 = "org.scalatest" %% "scalatest" % "3.2.0-SNAP10" % PerfTest
 
     val testDeps = Seq(junit, scalaCheck, scalaTest)
   }
 
-  import BuildSettings._
-  import Dependencies._
-  import Resolvers._
-  import UnidocKeys._
 
-  lazy val oscar = Project(
-    id = "oscar",
-    base = file("."),
-    settings =
-      commonSettings ++
-        packSettings ++
-        unidocSettings ++
-        Seq(libraryDependencies ++= testDeps) :+
-        (unidocProjectFilter in (ScalaUnidoc, unidoc) := inAnyProject -- inProjects(oscarFzn, oscarFznCbls, oscarFznCp, oscarPerf)),
-    aggregate = Seq(oscarAlgebra, oscarAlgo, oscarCbls, oscarCp, oscarCPXcsp3, oscarPerf, oscarModeling, oscarDfo, oscarUtil, oscarVisual, oscarFzn, oscarFznCbls, oscarFznCp, oscarDes, oscarInvariants)
-  )
-
-  lazy val oscarAlgebra = Project(
-    id = "oscar-algebra",
-    base = file("oscar-algebra"),
-    settings =
-      commonSettings ++
-        Seq(libraryDependencies ++= testDeps)
-  )
-
-  lazy val oscarAlgo = Project(
-    id = "oscar-algo",
-    base = file("oscar-algo"),
-    settings =
-      commonSettings ++
-        Seq(libraryDependencies ++= testDeps),
-    dependencies = Seq(oscarUtil, oscarVisual)
-  )
-
-  lazy val oscarCbls = Project(
-    id = "oscar-cbls",
-    base = file("oscar-cbls"),
-    settings =
-      commonSettings ++
-        packAutoSettings ++
-        Seq(
-          libraryDependencies ++= testDeps :+ scalaSwing,
-          packGenerateWindowsBatFile := false
-        ),
-    dependencies = Seq(oscarVisual)
-  )
-
-  lazy val oscarCp = Project(
-    id = "oscar-cp",
-    base = file("oscar-cp"),
-    settings =
-      commonSettings ++
-        Seq(libraryDependencies ++= testDeps :+ scalaParserCombinators),
-    dependencies = Seq(oscarAlgo, oscarVisual)
-  )
-
-  lazy val oscarModeling = Project(
-    id = "oscar-modeling",
-    base = file("oscar-modeling"),
-    settings =
-      commonSettings ++
-        Seq(
-          scalacOptions in Compile ++= Seq("-language:reflectiveCalls"),
-          resolvers ++= Seq(xypron),
-          libraryDependencies ++= testDeps :+ graphStreamCore :+ graphStreamAlgo :+ graphStreamUI :+ scallop
-                               :+ akkaActor :+ scalaSwing :+ jfreechart :+ jcommon),
-    dependencies = Seq(oscarCp)
-  )
-
-  lazy val oscarCPXcsp3 = Project(
-    id = "oscar-cp-xcsp3",
-    base = file("oscar-cp-xcsp3"),
-    settings =
-      commonSettings ++
-        Seq(
-          resolvers ++= Seq(ingi),
-          libraryDependencies ++= testDeps :+ xcsp3),
-    dependencies = Seq(oscarCp, oscarModeling)
-  )
-
-  lazy val oscarPerf = Project(
-    id = "oscar-perf",
-    base = file("oscar-perf"),
-    settings = commonSettings ++ Seq(resolvers ++= Seq(ingi), libraryDependencies ++= testDeps :+ xcsp3),
-    dependencies = Seq(oscarCp, oscarCPXcsp3, oscarModeling)
-  ).configs( PerfTest )
-    .settings(libraryDependencies ++= testDeps)
-    .settings(inConfig(PerfTest)(Defaults.testTasks ++ Seq()): _*)
-    .settings(inConfig(PerfTest)(baseDirectory in PerfTest := file(".")))
-    .settings(
-      testOptions in Test := Seq(Tests.Filter(x => !(x endsWith "PerfTest"))),
-      testOptions in PerfTest := Seq(Tests.Filter(_ endsWith "PerfTest"))
-    )
-
-
-  // Not included in the root build
-  lazy val oscarDes = Project(
-    id = "oscar-des",
-    base = file("oscar-des"),
-    settings =
-      commonSettings ++
-        Seq(libraryDependencies ++= testDeps :+ jsci),
-    dependencies = Seq(oscarInvariants)
-  )
-
-  lazy val oscarDfo = Project(
-    id = "oscar-dfo",
-    base = file("oscar-dfo"),
-    settings =
-      commonSettings ++
-        Seq(libraryDependencies ++= testDeps :+ jcommon :+ jfreechart),
-    dependencies = Seq(oscarAlgebra, oscarAlgo, oscarVisual)
-  )
-
-  // Not included in the default build
-  lazy val oscarFzn = Project(
-    id = "oscar-fzn",
-    base = file("oscar-fzn"),
-    settings =
-      commonSettings ++
-        Seq(libraryDependencies ++= testDeps :+ antlr4Runtime)
-  )
-
-  lazy val oscarFznCbls = Project(
-    id = "oscar-fzn-cbls",
-    base = file("oscar-fzn-cbls"),
-    settings =
-      commonSettings ++
-      Seq(libraryDependencies ++= testDeps),
-    dependencies = Seq(oscarCbls,oscarFzn,oscarFznCp)
-  )
-
-  lazy val oscarFznCp = Project(
-    id = "oscar-fzn-cp",
-    base = file("oscar-fzn-cp"),
-    settings =
-      commonSettings ++
-      Seq(libraryDependencies ++= testDeps),
-    dependencies = Seq(oscarCp,oscarFzn)
-  )
-
-
-  // Not included in the build
-  lazy val oscarInvariants = Project(
-    id = "oscar-invariants",
-    base = file("oscar-invariants"),
-    settings =
-      commonSettings ++
-        Seq(libraryDependencies ++= testDeps)
-  )
-
-  lazy val oscarUtil = Project(
-    id = "oscar-util",
-    base = file("oscar-util"),
-    settings =
-      commonSettings ++
-        Seq(libraryDependencies ++= testDeps :+ scalaXml)
-  )
-
-  lazy val oscarVisual = Project(
-    id = "oscar-visual",
-    base = file("oscar-visual"),
-    settings =
-      commonSettings ++
-        Seq(libraryDependencies ++= testDeps :+ jfreechart :+ swingx :+ swingxWs),
-    dependencies = Seq(oscarUtil)
-  )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,20 +1,11 @@
 resolvers += Resolver.url("artifactory", url("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns)
 
+addSbtPlugin("com.github.sbt" % "sbt-jacoco" % "3.0.1")
 
-libraryDependencies ++= Seq(
-  "org.jacoco" % "org.jacoco.core" % "0.5.9.201207300726" artifacts Artifact("org.jacoco.core", "jar", "jar"),
-  "org.jacoco" % "org.jacoco.report" % "0.5.9.201207300726" artifacts Artifact("org.jacoco.report", "jar", "jar"))
+addSbtPlugin("org.xerial.sbt" % "sbt-pack" % "0.10.1")//"0.6.8")
 
-addSbtPlugin("de.johoop" % "jacoco4sbt" % "2.1.6")
+addSbtPlugin("com.eed3si9n" % "sbt-unidoc" % "0.4.1")
 
-addSbtPlugin("org.xerial.sbt" % "sbt-pack" % "0.6.8")
-
-addSbtPlugin("com.eed3si9n" % "sbt-unidoc" % "0.3.0")
-
-addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.5.0")
-
-addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0")
-
-addSbtPlugin("me.lessis" % "bintray-sbt" % "0.3.0")
+addSbtPlugin("org.foundweekends" % "sbt-bintray" % "0.5.1")
 
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.1")
