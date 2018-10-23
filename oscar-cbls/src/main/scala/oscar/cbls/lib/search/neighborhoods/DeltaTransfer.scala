bd6f9763634ad877ff800d7e764774f223158e57
Renaud De Landtsheer <renaud.delandtsheer@cetic.be>
2018-10-23 15:20:21 +0200

    drafting some neighborhoods

---
+++ oscar-cbls/src/main/scala/oscar/cbls/lib/search/neighborhoods/DeltaTransfer.scala
@@ -0,0 +1,120 @@
+package oscar.cbls.lib.search.neighborhoods
+
+import oscar.cbls._
+import oscar.cbls.algo.search.{HotRestart, IdenticalAggregator}
+import oscar.cbls.core.computation.{CBLSIntVar, Variable}
+import oscar.cbls.core.search.{EasyNeighborhoodMultiLevel, First, LoopBehavior, Move}
+
+
+case class DeltaTransfer(vars:Array[CBLSIntVar],
+                         name:String = "SwapsNeighborhood",
+                         searchZone1:()=>Iterable[Int] = null,
+                         searchZone2:() => (Int,Int)=>Iterable[Int] = null,
+                         searchZoneForDelta:() => (Int,Int) => (Int,Int) => Iterable[Int], //donne des delta à essayer (TOTO: faire un enwton raphson ou regula falsi ou dichotomoe ici!!!
+                         symmetryCanBeBrokenOnIndices:Boolean = true,
+                         selectFirstVariableBehavior:LoopBehavior = First(),
+                         selectSecondVariableBehavior:LoopBehavior = First(),
+                         selectDeltaBehavior:LoopBehavior = First(),
+                         hotRestart:Boolean = true)extends EasyNeighborhoodMultiLevel[TransferMove](name){
+  //the indice to start with for the exploration
+  var indiceOfFirstVariable:Int = 0
+  override def exploreNeighborhood() {
+
+    val firstIterationSchemeZone =
+      if (searchZone1 == null) {
+        if (hotRestart) {
+          if (indiceOfFirstVariable >= vars.length) indiceOfFirstVariable = 0
+          vars.indices startBy indiceOfFirstVariable
+        } else vars.indices
+      } else if (hotRestart) HotRestart(searchZone1(), indiceOfFirstVariable) else searchZone1()
+
+    val searchZone2ForThisSearch = if (searchZone2 == null) null else searchZone2()
+    val searchZoneForDeltaL1 = if(searchZoneForDelta == null) null else searchZoneForDelta()
+
+    val (iIterator,notifyFound1) = selectFirstVariableBehavior.toIterator(firstIterationSchemeZone)
+    while (iIterator.hasNext) {
+      indiceOfFirstVariable = iIterator.next()
+
+      val firstVar = vars(indiceOfFirstVariable)
+      val oldValOfFirstVar = firstVar.newValue
+
+      val secondIterationSchemeZone = if (searchZone2ForThisSearch == null) vars.indices else searchZone2ForThisSearch(indiceOfFirstVariable,oldValOfFirstVar)
+      val searchZoneForDeltaL2 = if(searchZoneForDeltaL1 == null) null else searchZoneForDeltaL1(indiceOfFirstVariable,oldValOfFirstVar)
+
+      val (jIterator,notifyFound2) = selectSecondVariableBehavior.toIterator(secondIterationSchemeZone)
+      while (jIterator.hasNext) {
+        val j = jIterator.next()
+        val secondVar = vars(j)
+        val oldValOfSecondVar = secondVar.newValue
+
+        if ((!symmetryCanBeBrokenOnIndices || indiceOfFirstVariable < j) //we break symmetry on variables
+          && indiceOfFirstVariable != j
+          && secondVar.domain.contains(oldValOfFirstVar)
+          && firstVar.domain.contains(oldValOfSecondVar)) {
+
+          this.firstVar = firstVar
+          this.secondVar = secondVar
+          this.firstVarIndice = indiceOfFirstVariable
+          this.secondVarIndice = j
+
+          val searchZoneForDeltaL3 = if(searchZoneForDeltaL2 == null) null else searchZoneForDeltaL2(secondVarIndice,oldValOfSecondVar)
+
+          val iterationOnDelta = if (searchZoneForDeltaL3 == null) ??? else searchZoneForDeltaL3
+          //TODO: il faut cadrer dans le domaine des variables!!
+
+          val (deltaIterator,notifyFound3) = selectDeltaBehavior.toIterator(iterationOnDelta)
+          while (deltaIterator.hasNext) {
+            val delta = deltaIterator.next()
+
+            this.delta = delta
+
+            if (evaluateCurrentMoveObjTrueIfSomethingFound(obj.swapVal(firstVar, secondVar))) {
+              notifyFound1()
+              notifyFound2()
+              notifyFound3()
+            }
+          }
+        }
+      }
+    }
+    indiceOfFirstVariable = indiceOfFirstVariable +1
+  }
+
+  var firstVar:CBLSIntVar = null
+  var secondVar:CBLSIntVar = null
+  var firstVarIndice:Int = 0
+  var secondVarIndice:Int = 0
+  var delta:Int = 0
+
+  override def instantiateCurrentMove(newObj: Int) =
+    TransferMove(firstVar, secondVar, delta:Int, firstVarIndice,secondVarIndice,newObj, name)
+
+  //this resets the internal state of the Neighborhood
+  override def reset(): Unit = {
+    indiceOfFirstVariable = 0
+  }
+}
+
+
+/** standard move that swaps the value of two CBLSIntVar
+  *
+  * @param i the variable
+  * @param j the other variable
+  * @param objAfter the objective after this assignation will be performed
+  * @param neighborhoodName a string describing the neighborhood hat found the move (for debug purposes)
+  * @author renaud.delandtsheer@cetic.be
+  */
+case class TransferMove(i:CBLSIntVar,j:CBLSIntVar, delta:Int, idI:Int, idJ:Int, override val objAfter:Int, override val neighborhoodName:String = null)
+  extends Move(objAfter, neighborhoodName){
+
+  override def commit() {
+    i :+= delta
+    j:-= delta
+  }
+
+  override def toString: String  = {
+    neighborhoodNameToString + "Transfer(" + i + " :+= " + delta + " "  + j + ":-=" + delta + objToString + ")"
+  }
+
+  override def touchedVariables: List[Variable] = List(i,j)
+}
---
+++ oscar-cbls/src/main/scala/oscar/cbls/lib/search/neighborhoods/Gradient.scala
@@ -0,0 +1,38 @@
+/*******************************************************************************
+  * OscaR is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU Lesser General Public License as published by
+  * the Free Software Foundation, either version 2.1 of the License, or
+  * (at your option) any later version.
+  *
+  * OscaR is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU Lesser General Public License  for more details.
+  *
+  * You should have received a copy of the GNU Lesser General Public License along with OscaR.
+  * If not, see http://www.gnu.org/licenses/lgpl-3.0.en.html
+  ******************************************************************************/
+
+package oscar.cbls.lib.search.neighborhoods
+
+import oscar.cbls.core.computation.CBLSIntVar
+import oscar.cbls.core.search.{EasyNeighborhoodMultiLevel, First, LoopBehavior}
+
+case class GradientDescent(vars:Array[CBLSIntVar],
+                           name:String = "GradientDescent",
+                           selectedVars:() => Iterable[Int] = null,
+                           domain:(CBLSIntVar,Int) => Iterable[Int] = (v,i) => v.domain,
+                           hotRestart:Boolean = true)
+  extends EasyNeighborhoodMultiLevel[AssignMove](name) {
+
+
+  //step1: interroger le gradient dans toutes les directions de selectedVars
+
+
+  //step2: newton-raphson pour trouver le pas
+
+
+
+
+}
+
