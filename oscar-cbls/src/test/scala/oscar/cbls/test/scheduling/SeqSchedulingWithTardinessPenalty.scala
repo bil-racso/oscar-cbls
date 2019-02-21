f58e5f093030168cfa37d3abaf49811e476dd64f
Gustavo Ospina <gustavo.ospina@cetic.be>
2019-02-21 10:45:50 +0100

    Modified example of tardiness

--- oscar-cbls/src/test/scala/oscar/cbls/test/scheduling/SeqSchedulingWithTardinessPenalty.scala
+++ oscar-cbls/src/test/scala/oscar/cbls/test/scheduling/SeqSchedulingWithTardinessPenalty.scala
@@ -12,55 +12,36 @@ object SeqSchedulingWithTardinessPenalty {
   val m = new Store()
 
   // Activities
-  val analysis = new Activity(m, "Analysis", 10)
-  val design = new Activity(m, "Design", 10)
-  val coding = new Activity(m, "Coding", 15)
-  val test = new Activity(m, "Testing", 25)
-  val qa = new Activity(m, "Quality control", 50)
-  val pm = new Activity(m, "Project Management", 60)
+  val a1 = new Activity(m, "A1", 10)
+  val a2 = new Activity(m, "A2", 10)
+  val a3 = new Activity(m, "A3", 10)
+  val a4 = new Activity(m, "A4", 10)
+  val a5 = new Activity(m, "A5", 10)
 
-  val activities = new BoundedArray[Activity](6, Activity.setIndex)
-  activities.:::(List(qa, design, analysis, test, coding, pm))
+  val activities = new BoundedArray[Activity](5, Activity.setIndex)
+  activities.:::(List(a1, a2, a3, a4, a5))
 
   // Running modes
-  val runningModesForAnalysisQAPM = new RunningModeResources(2)
+  val runningModesForAnalysisQAPM = new RunningModeResources(1)
   val rm_analysis = new RunningMode("RM_ANALYSIS", 0)
-  val rm_qa_pm = new RunningMode("RM_QA_PM", 0)
   runningModesForAnalysisQAPM.addRunningMode(rm_analysis, true)
-  runningModesForAnalysisQAPM.addRunningMode(rm_qa_pm)
-  runningModesForAnalysisQAPM.addSetupTime(rm_analysis, rm_qa_pm, 1)
-  runningModesForAnalysisQAPM.addSetupTime(rm_qa_pm, rm_analysis, 1)
-
-  val runningModesForDevTest = new RunningModeResources(2)
-  val rm_dev = new RunningMode("RM_DEV", 0)
-  val rm_test = new RunningMode("RM_TEST", 0)
-  runningModesForDevTest.addRunningMode(rm_dev,true)
-  runningModesForDevTest.addRunningMode(rm_test)
-  runningModesForDevTest.addSetupTime(rm_dev, rm_test, 2)
-  runningModesForDevTest.addSetupTime(rm_test, rm_dev, 2)
 
   // Resources
-  val res_analyst = new Resource(m, "AnalystQAPM", 5, runningModesForAnalysisQAPM)
-  val res_senior_dev_test = new Resource(m, "Senior Dev/test", 2, runningModesForDevTest)
+  val res_analyst = new Resource(m, "AnalystQAPM", 1, runningModesForAnalysisQAPM)
 
-  val resources = new BoundedArray[Resource](2, Resource.setIndex)
-  resources.:::(List(res_analyst, res_senior_dev_test))
+  val resources = new BoundedArray[Resource](1, Resource.setIndex)
+  resources.:::(List(res_analyst))
 
   // Resource usage
-  val resUsages = new ActivityResourceUsages(6, 2)  // parameters looks strange, why not activity and resource list ?
-  resUsages.addActivityResourceUsage(analysis, res_analyst, rm_analysis, 2)
-  resUsages.addActivityResourceUsage(design, res_analyst, rm_analysis, 1)
-  resUsages.addActivityResourceUsage(coding, res_senior_dev_test, rm_dev, 2)
-  resUsages.addActivityResourceUsage(test, res_senior_dev_test, rm_test, 1)
-  resUsages.addActivityResourceUsage(qa, res_analyst, rm_analysis, 1)
-  resUsages.addActivityResourceUsage(pm, res_analyst, rm_analysis, 1)
+  val resUsages = new ActivityResourceUsages(5, 1)  // parameters looks strange, why not activity and resource list ?
+  resUsages.addActivityResourceUsage(a1, res_analyst, rm_analysis, 1)
+  resUsages.addActivityResourceUsage(a2, res_analyst, rm_analysis, 1)
+  resUsages.addActivityResourceUsage(a3, res_analyst, rm_analysis, 1)
+  resUsages.addActivityResourceUsage(a4, res_analyst, rm_analysis, 1)
+  resUsages.addActivityResourceUsage(a5, res_analyst, rm_analysis, 1)
 
   // Precedences
-  val precedences = new Precedences(6)
-  precedences.addPrecedence(analysis, design)
-  precedences.addPrecedence(design, coding)
-  precedences.addPrecedence(coding, test)
-  precedences.addPrecedence(analysis, qa)
+  val precedences = new Precedences(5) // no precedences
 
   // Scheduling Problem
   val scProblem = new SchedulingProblem(m, activities, resources, precedences, resUsages)
@@ -68,9 +49,9 @@ object SeqSchedulingWithTardinessPenalty {
   // Tardiness variables
   val tardinessPenalty = 100
   val activitiesTardiness = Array.tabulate(activities.size)(i => {
-    scProblem.startTimes(i)*tardinessPenalty
+    scProblem.startTimes(i)*tardinessPenalty*i
   })
-  val globalTardiness:Objective = Sum(activitiesTardiness)
+  val globalTardiness: Objective = Sum(activitiesTardiness)
 
   // Model closed
   m.close()
