cb375c0b77ce9bf222a38e8e8575ba6b5d80cb19
Germeau Fabian <devnull@localhost>
2019-02-28 16:13:54 +0100

    Adapt routing examples to 64bits structure + enhance routing map display

--- oscar-cbls/src/main/examples/oscar/examples/cbls/routing/SimpleVRPWithMaxDetours.scala
+++ oscar-cbls/src/main/examples/oscar/examples/cbls/routing/SimpleVRPWithMaxDetours.scala
@@ -1,5 +1,5 @@
 package oscar.examples.cbls.routing
-/*
+
 import oscar.cbls._
 import oscar.cbls.business.routing._
 
@@ -20,7 +20,7 @@ object SimpleVRPWithMaxDetours extends App{
   TimeWindowHelper.reduceTimeWindows(myVRP,travelDurationMatrix,maxTravelDurations,earliestArrivalTimes,latestLeavingTimes,taskDurations)
 
   // Distance
-  val totalRouteLength = constantRoutingDistance(myVRP.routes,n,v,false,symmetricDistance,true,true,false)(0)
+  val totalRouteLength = routeLength(myVRP.routes,n,v,false,symmetricDistance,true,true,false)(0)
 
   //Chains
   val precedenceRoute = myVRP.routes.createClone()
@@ -61,12 +61,12 @@ object SimpleVRPWithMaxDetours extends App{
   //Time window constraints
   val arrivalTimesAtEnd = timeWindowInvariant.content1AtEnd
   for(i <- 0 until n){
-    if(i < v && latestLeavingTimes(i) != Int.MaxValue) {
+    if(i < v && latestLeavingTimes(i) != Long.MaxValue) {
       timeWindowConstraints.post((arrivalTimesAtEnd(i) le latestLeavingTimes(i)).nameConstraint("end of time for vehicle " + i))
     } else {
-      if(latestLeavingTimes(i) != Int.MaxValue)
+      if(latestLeavingTimes(i) != Long.MaxValue)
         timeWindowConstraints.post((leaveTimes(i) le latestLeavingTimes(i)).nameConstraint("end of time window on node " + i))
-      if(maxWaitingDurations(i) != Int.MaxValue)
+      if(maxWaitingDurations(i) != Long.MaxValue)
         timeWindowConstraints.post((arrivalTimes(i) ge earliestArrivalTimes(i)).nameConstraint("start of time window on node (with duration)" + i))
     }
   }
@@ -81,14 +81,14 @@ object SimpleVRPWithMaxDetours extends App{
 
   val relevantPredecessorsOfNodes = TimeWindowHelper.relevantPredecessorsOfNodes(myVRP, timeWindowExtension, travelDurationMatrix)
   val relevantSuccessorsOfNodes = TimeWindowHelper.relevantSuccessorsOfNodes(myVRP, timeWindowExtension, travelDurationMatrix)
-  val closestRelevantNeighborsByDistance = Array.tabulate(n)(DistanceHelper.lazyClosestPredecessorsOfNode(symmetricDistance,relevantPredecessorsOfNodes))
+  val closestRelevantNeighborsByDistance = Array.tabulate(n)(DistanceHelper.lazyClosestPredecessorsOfNode(symmetricDistance,relevantPredecessorsOfNodes)(_))
 
-  def relevantPredecessorsForLastNode(lastNode: Int) = ChainsHelper.relevantNeighborsForLastNodeAfterHead(myVRP,chainsExtension,Some(relevantPredecessorsOfNodes(lastNode)))(lastNode)
+  def relevantPredecessorsForLastNode(lastNode: Long) = ChainsHelper.relevantNeighborsForLastNodeAfterHead(myVRP,chainsExtension,Some(relevantPredecessorsOfNodes(lastNode)))(lastNode)
   val relevantPredecessorsForInternalNodes = ChainsHelper.computeRelevantNeighborsForInternalNodes(myVRP, chainsExtension)_
 
-  def postFilter(node:Int): (Int) => Boolean = {
+  def postFilter(node:Long): (Long) => Boolean = {
     val routedNode = myVRP.routed.value
-    (neighbor: Int) => {
+    (neighbor: Long) => {
       val successor = myVRP.nextNodeOf(neighbor)
       routedNode.contains(neighbor) &&
         (successor.isEmpty || relevantSuccessorsOfNodes(node).contains(successor.get))
@@ -98,12 +98,12 @@ object SimpleVRPWithMaxDetours extends App{
 
   // MOVING
   val nextMoveGenerator = {
-    (exploredMoves:List[OnePointMoveMove], t:Option[List[Int]]) => {
-      val chainTail: List[Int] = t match {
+    (exploredMoves:List[OnePointMoveMove], t:Option[List[Long]]) => {
+      val chainTail: List[Long] = t match {
         case None =>
           val movedNode = exploredMoves.head.movedPoint
           chainsExtension.nextNodesInChain(chainsExtension.firstNodeInChainOfNode(movedNode))
-        case Some(tail: List[Int]) => tail
+        case Some(tail: List[Long]) => tail
       }
       chainTail match {
         case Nil => None
@@ -116,35 +116,35 @@ object SimpleVRPWithMaxDetours extends App{
     }
   }
 
-  val firstNodeOfChainMove = onePointMove(() => myVRP.routed.value.filter(chainsExtension.isHead),()=> myVRP.kFirst(v*2,closestRelevantNeighborsByDistance,postFilter), myVRP,neighborhoodName = "MoveHeadOfChain")
+  val firstNodeOfChainMove = onePointMove(() => myVRP.routed.value.filter(chainsExtension.isHead),()=> myVRP.kFirst(v*2,closestRelevantNeighborsByDistance(_),postFilter), myVRP,neighborhoodName = "MoveHeadOfChain")
 
-  def lastNodeOfChainMove(lastNode:Int) = onePointMove(() => List(lastNode),()=> myVRP.kFirst(v*2,relevantPredecessorsForLastNode,postFilter), myVRP,neighborhoodName = "MoveLastOfChain")
+  def lastNodeOfChainMove(lastNode:Long) = onePointMove(() => List(lastNode),()=> myVRP.kFirst(v*2,relevantPredecessorsForLastNode,postFilter), myVRP,neighborhoodName = "MoveLastOfChain")
 
   val oneChainMove = {
     dynAndThen(firstNodeOfChainMove,
       (moveMove: OnePointMoveMove) => {
-        mu[OnePointMoveMove, Option[List[Int]]](
+        mu[OnePointMoveMove, Option[List[Long]]](
           lastNodeOfChainMove(chainsExtension.lastNodeInChainOfNode(moveMove.movedPoint)),
           nextMoveGenerator,
           None,
-          Int.MaxValue,
+          Long.MaxValue,
           false)
       }
     ) name "One Chain Move"
 
   }
 
-  def onePtMove(k:Int) = profile(onePointMove(myVRP.routed, () => myVRP.kFirst(k,closestRelevantNeighborsByDistance,postFilter), myVRP)) name "One Point Move"
+  def onePtMove(k:Long) = profile(onePointMove(myVRP.routed, () => myVRP.kFirst(k,closestRelevantNeighborsByDistance(_),postFilter), myVRP)) name "One Point Move"
 
 
   // INSERTING
   val nextInsertGenerator = {
-    (exploredMoves:List[InsertPointMove], t:Option[List[Int]]) => {
-      val chainTail: List[Int] = t match {
+    (exploredMoves:List[InsertPointMove], t:Option[List[Long]]) => {
+      val chainTail: List[Long] = t match {
         case None =>
           val insertedNode = exploredMoves.head.insertedPoint
           chainsExtension.nextNodesInChain(chainsExtension.firstNodeInChainOfNode(insertedNode))
-        case Some(tail: List[Int]) => tail
+        case Some(tail: List[Long]) => tail
       }
 
       chainTail match {
@@ -158,18 +158,18 @@ object SimpleVRPWithMaxDetours extends App{
     }
   }
 
-  val firstNodeOfChainInsertion = insertPointUnroutedFirst(() => chainsExtension.heads.filter(n => !myVRP.isRouted(n)),()=> myVRP.kFirst(v*2,closestRelevantNeighborsByDistance, postFilter), myVRP,neighborhoodName = "InsertUF")
+  val firstNodeOfChainInsertion = insertPointUnroutedFirst(() => chainsExtension.heads.filter(n => !myVRP.isRouted(n)),()=> myVRP.kFirst(v*2,closestRelevantNeighborsByDistance(_), postFilter), myVRP,neighborhoodName = "InsertUF")
 
-  def lastNodeOfChainInsertion(lastNode:Int) = insertPointUnroutedFirst(() => List(lastNode),()=> myVRP.kFirst(v*2,relevantPredecessorsForLastNode,postFilter), myVRP,neighborhoodName = "InsertUF")
+  def lastNodeOfChainInsertion(lastNode:Long) = insertPointUnroutedFirst(() => List(lastNode),()=> myVRP.kFirst(v*2,relevantPredecessorsForLastNode,postFilter), myVRP,neighborhoodName = "InsertUF")
 
   val oneChainInsert = {
     dynAndThen(firstNodeOfChainInsertion,
       (insertMove: InsertPointMove) => {
-        mu[InsertPointMove,Option[List[Int]]](
+        mu[InsertPointMove,Option[List[Long]]](
           lastNodeOfChainInsertion(chainsExtension.lastNodeInChainOfNode(insertMove.insertedPoint)),
           nextInsertGenerator,
           None,
-          Int.MaxValue,
+          Long.MaxValue,
           false)
       }) name "One Chain Insert"
 
@@ -185,4 +185,3 @@ object SimpleVRPWithMaxDetours extends App{
 
   search.profilingStatistics
 }
-*/
---
+++ oscar-cbls/src/main/scala/oscar/cbls/visual/routing/BasicRoutingMap.scala
@@ -0,0 +1,116 @@
+package oscar.cbls.visual.routing
+
+/**
+  * *****************************************************************************
+  * OscaR is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU Lesser General Public License as published by
+  * the Free Software Foundation, either version 2.1 of the License, or
+  * (at your option) any later version.
+  *
+  * OscaR is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU Lesser General Public License  for more details.
+  *
+  * You should have received a copy of the GNU Lesser General Public License along with OscaR.
+  * If not, see http://www.gnu.org/licenses/lgpl-3.0.en.html
+  * ****************************************************************************
+  */
+
+import java.awt.geom.Line2D.Double
+import java.awt.Color
+
+import oscar.cbls._
+import oscar.cbls.business.routing.model.VRP
+import oscar.cbls.util.StopWatch
+import oscar.visual.VisualDrawing
+import oscar.visual.shapes.{VisualCircle, VisualLine, VisualShape}
+
+
+/**
+  * @author fabian.germeau@student.vinci.be
+  */
+
+class BasicRoutingMap(vrp: VRP,
+                      nodesPositions: Array[(scala.Double,scala.Double)],
+                      colorValues: Array[Color],
+                      mapSize: Option[Long],
+                      refreshRate: Long) extends VisualDrawing(false,false) with StopWatch with RoutingMapTrait {
+
+  private lazy val pixelPositionOfNodes: Array[((scala.Double,scala.Double),Int)] = positionsToPixels()
+  private lazy val points:Array[VisualCircle] = buildPoints()
+  private lazy val lines:Array[VisualLine] = buildLines()
+  private val mapSizeNow = mapSize.getOrElse(computeSize(nodesPositions))
+
+  private var lastRefresh = 0L
+
+  override def addShape(shape: VisualShape, repaintAfter: Boolean = true): Unit ={
+    super.addShape(shape,false)
+  }
+
+  private def buildPoints() ={
+    pixelPositionOfNodes.map(position_node => {
+      val position = position_node._1
+      val node = position_node._2
+      if(node < vrp.v){
+        val tempPoint = new VisualCircle(this,position._1.toInt,position._2.toInt,5)
+        tempPoint.innerCol_$eq(colorValues(node))
+        tempPoint
+      }
+      else{
+        val tempPoint = new VisualCircle(this,position._1.toInt,position._2.toInt,1)
+        tempPoint.innerCol_$eq(Color.black)
+        tempPoint
+      }
+    })
+  }
+
+  private def buildLines(): Array[VisualLine] ={
+    pixelPositionOfNodes.map(position_node => {
+      val position = position_node._1
+      val line = new VisualLine(this,
+        new Double(position._1, position._2, position._1, position._2))
+      line.borderWidth = 2
+      line
+    })
+  }
+
+  def drawRoutes(force: Boolean): Unit ={
+    val currentTime = getWatch
+    if(force || currentTime - lastRefresh >= refreshRate) {
+      val routes = Array.tabulate(vrp.v)(v => vrp.getRouteOfVehicle(v))
+
+      for (r <- 0 until vrp.v) {
+        val color = colorValues(r)
+        var previousPoint = routes(r).head
+        for (p <- routes(r).drop(1)) {
+          lines(longToInt(previousPoint)).outerCol_$eq(color)
+          lines(longToInt(previousPoint)).dest = (points(p).getX, points(p).getY)
+          previousPoint = p
+        }
+        lines(longToInt(previousPoint)).outerCol_$eq(color)
+        lines(longToInt(previousPoint)).dest = (points(r).getX, points(r).getY)
+      }
+      lastRefresh = currentTime
+    }
+  }
+
+  def clear(): Unit ={
+    super.clear()
+  }
+
+  private def positionsToPixels(): Array[((scala.Double, scala.Double),Int)] = {
+    Array.tabulate(vrp.n)(index => {
+      ((nodesPositions(index)._1*getHeight/mapSizeNow, nodesPositions(index)._2*getHeight/mapSizeNow),index)
+    })
+  }
+
+  private def computeSize(nodesPositions: Array[(scala.Double,scala.Double)]): Long ={
+    val distanceBetweenNodes = Array.tabulate(vrp.n)(x =>
+      Array.tabulate(vrp.n)(y =>
+        List(nodesPositions(x)._1 - nodesPositions(y)._1,nodesPositions(x)._2 - nodesPositions(y)._2))).
+      flatten
+    val maxDistance = distanceBetweenNodes.flatten.max
+    (maxDistance*1.05).toInt
+  }
+}
