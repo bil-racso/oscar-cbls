7f887285622ae1e1d45534141780511dd7b06a01
Renaud De Landtsheer <renaud.delandtsheer@cetic.be>
2019-03-11 11:46:57 +0100

    moved invariant to proper place

--- oscar-cbls/src/main/scala/oscar/cbls/business/routing/invariants/WeithedNodesPerVehicle.scala
+++ oscar-cbls/src/main/scala/oscar/cbls/business/routing/invariants/WeithedNodesPerVehicle.scala
@@ -1,10 +1,10 @@
-package oscar.cbls.business.routing
+package oscar.cbls.business.routing.invariants
 
 import oscar.cbls
-import oscar.cbls.{CBLSIntVar, Variable}
 import oscar.cbls.algo.seq.{IntSequence, IntSequenceExplorer}
 import oscar.cbls.business.routing.invariants.group._
 import oscar.cbls.core.computation.ChangingSeqValue
+import oscar.cbls.{CBLSIntVar, Variable}
 
 
 object WeigthedNodesPerVehicle{
