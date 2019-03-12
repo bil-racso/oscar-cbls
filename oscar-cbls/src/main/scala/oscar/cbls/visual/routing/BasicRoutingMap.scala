410a2d27e3d1e05d8ccc0a56749e4a05153a3781
Germeau Fabian <devnull@localhost>
2019-03-12 13:04:15 +0100

    Add toolTip information to display custom message when passing over a customer or depot. Working on real and basic routing map. Basic information are displayed by default.

--- oscar-cbls/src/main/scala/oscar/cbls/visual/routing/BasicRoutingMap.scala
+++ oscar-cbls/src/main/scala/oscar/cbls/visual/routing/BasicRoutingMap.scala
@@ -19,6 +19,7 @@ package oscar.cbls.visual.routing
 
 import java.awt.geom.Line2D.Double
 import java.awt.Color
+import java.awt.geom.Point2D
 
 import oscar.cbls._
 import oscar.cbls.business.routing.model.VRP
@@ -35,11 +36,13 @@ class BasicRoutingMap(vrp: VRP,
                       nodesPositions: Array[(scala.Double,scala.Double)],
                       colorValues: Array[Color],
                       mapSize: Option[Long],
-                      refreshRate: Long) extends VisualDrawing(false,false) with StopWatch with RoutingMapTrait {
+                      refreshRate: Long,
+                      toolTipInfo: Option[Int => Option[String]]) extends VisualDrawing(false,false) with StopWatch with RoutingMapTrait {
 
   private lazy val pixelPositionOfNodes: Array[((scala.Double,scala.Double),Int)] = positionsToPixels()
   private lazy val points:Array[VisualCircle] = buildPoints()
   private lazy val lines:Array[VisualLine] = buildLines()
+  private val toolTips:Array[String] = Array.fill(vrp.n)("")
   private val mapSizeNow = mapSize.getOrElse(computeSize(nodesPositions))
 
   private var lastRefresh = 0L
@@ -48,33 +51,68 @@ class BasicRoutingMap(vrp: VRP,
     super.addShape(shape,false)
   }
 
+  /**
+    * This method creates the VisualCircle that will be displayed.
+    * They represent depots (circle of size 5.0) and customers (circle of size 1.0)
+    * @return
+    */
   private def buildPoints() ={
     pixelPositionOfNodes.map(position_node => {
       val position = position_node._1
       val node = position_node._2
+      // Setting starting information for each node
+      toolTips(node) = generateToolTipInfo(node)
+
       if(node < vrp.v){
-        val tempPoint = new VisualCircle(this,position._1,position._2,5.0)
+        val tempPoint = new VisualCircle(this,position._1,position._2,5.0){
+          override def showToolTip(mousePoint: Point2D) = {
+            if (shape.contains(drawing.invertTransform(mousePoint))) {
+              drawing.showToolTip(toolTips(node))
+            }
+          }
+        }
         tempPoint.innerCol_$eq(colorValues(node))
+        tempPoint.toolTip_$eq(toolTips(node))
         tempPoint
       }
       else{
-        val tempPoint = new VisualCircle(this,position._1,position._2,1.0)
+        val tempPoint = new VisualCircle(this,position._1,position._2,2.0){
+          override def showToolTip(mousePoint: Point2D) = {
+            if (shape.contains(drawing.invertTransform(mousePoint))) {
+              drawing.showToolTip(toolTips(node))
+            }
+          }
+        }
         tempPoint.innerCol_$eq(Color.black)
+        tempPoint.toolTip_$eq(toolTips(node))
         tempPoint
       }
     })
   }
 
+  /**
+    * This methods build the VisualLine that will be displayed.
+    * They represent the road of each node to his successor.
+    * If the node has no successor, the destination of this line is the node itself.
+    * @return
+    */
   private def buildLines(): Array[VisualLine] ={
     pixelPositionOfNodes.map(position_node => {
       val position = position_node._1
       val line = new VisualLine(this,
         new Double(position._1, position._2, position._1, position._2))
-      line.borderWidth = 2
+      line.borderWidth = 3
       line
     })
   }
 
+  /**
+    * This method draw the route of each vehicle depending on the specified refresh rate.
+    * It simply modify the VisualLine of each encountered node by changing his destination.
+    * This avoid re-creating each component much faster than the previous version.
+    *
+    * @param force Force the route drawing (last drawing after the end of search for instance)
+    */
   def drawRoutes(force: Boolean): Unit ={
     val currentTime = getWatch
     if(force || currentTime - lastRefresh >= refreshRate) {
@@ -83,10 +121,13 @@ class BasicRoutingMap(vrp: VRP,
       for (r <- 0 until vrp.v) {
         val color = colorValues(r)
         var previousPoint = routes(r).head
+        var positionCounter = 1
         for (p <- routes(r).drop(1)) {
           lines(longToInt(previousPoint)).outerCol_$eq(color)
           lines(longToInt(previousPoint)).dest = (points(p).getX, points(p).getY)
+          toolTips(p) = generateToolTipInfo(p,r,positionCounter)
           previousPoint = p
+          positionCounter += 1
         }
         lines(longToInt(previousPoint)).outerCol_$eq(color)
         lines(longToInt(previousPoint)).dest = (points(r).getX, points(r).getY)
@@ -95,6 +136,7 @@ class BasicRoutingMap(vrp: VRP,
       for(unroutedNode <- vrp.unroutedNodes){
         lines(longToInt(unroutedNode)).outerCol_$eq(Color.black)
         lines(longToInt(unroutedNode)).dest = (points(unroutedNode).getX,points(unroutedNode).getY)
+        toolTips(unroutedNode) = generateToolTipInfo(unroutedNode)
       }
 
       lastRefresh = currentTime
@@ -118,5 +160,23 @@ class BasicRoutingMap(vrp: VRP,
       flatten
     val maxDistance = distanceBetweenNodes.flatten.max
     (maxDistance*1.05).toInt
+  }
+
+  /**
+    * This method aggregate the information attached to each node using basic node information (in this method)
+    * and the specialized information wanted by the user (toolTipInfo)
+    * @return
+    */
+  private def generateToolTipInfo(node: Int, vehicle: Int = vrp.n, position: Int = vrp.n): String ={
+    val defaultString =
+      if(node < vrp.v)
+        "Depot of vehicle " + vehicle + "\n"
+      else if(vehicle == vrp.n)
+        "Unrouted node " + node + "\n"
+      else
+        "Node " + node + " at the " + position + "th position of the vehicle " + vehicle + "\n"
+
+    defaultString +
+      (if(toolTipInfo.isDefined) toolTipInfo.get(node).getOrElse("") else "")
   }
 }
