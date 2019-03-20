e95dbff6ddd7660018c09f0928eb4d7389806148
Renaud De Landtsheer <renaud.delandtsheer@cetic.be>
2019-03-20 16:14:45 +0100

    Messy things that were not cleaned

--- oscar-cbls/src/main/examples/oscar/examples/cbls/routing/VRPWithWeightedNodes.scala
+++ oscar-cbls/src/main/examples/oscar/examples/cbls/routing/VRPWithWeightedNodes.scala
@@ -2,7 +2,7 @@ package oscar.examples.cbls.routing
 
 import oscar.cbls._
 import oscar.cbls.business.routing._
-import oscar.cbls.business.routing.invariants.WeigthedNodesPerVehicle
+import oscar.cbls.business.routing.invariants.WeightedNodesPerVehicle
 import oscar.cbls.core.search.{Best, First}
 import oscar.cbls.visual.routing.RoutingMapTypes
 
@@ -57,7 +57,7 @@ class VRPWithWeightedNodes(n: Int, v: Int, minLat: Double, maxLat: Double, minLo
   // The sum of node's weight can't excess the capacity of a vehicle
   val weightPerVehicle = Array.tabulate(v)(_ => CBLSIntVar(store))
   // This invariant maintains the total node's weight encountered by each vehicle
-  val weightedNodesConstraint = WeigthedNodesPerVehicle(myVRP.routes, v, nodeWeight, weightPerVehicle)
+  val weightedNodesConstraint = WeightedNodesPerVehicle(myVRP.routes, v, nodeWeight, weightPerVehicle)
   // This invariant maintains the capacity violation of each vehicle (le means lesser or equals)
   val vehicleCapacityViolation = Array.tabulate(v)(vehicle => (weightPerVehicle(vehicle) le vehicleCapacity(vehicle)))
   val constraintSystem = new ConstraintSystem(store)
